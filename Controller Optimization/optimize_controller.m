d79fad923061e9beba41bc7876169ca84e28ebe0
invertedparadox <demetrius@users.noreply.github.com>
2022-02-19 18:30:10 -0500

    Update optimize_controller.m

--- Controller Optimization/optimize_controller.m
+++ Controller Optimization/optimize_controller.m
@@ -7,10 +7,10 @@ num1 = length(ts);
 num2 = length(os);
 num3 = length(t_vary);
 
-exit_flag = zeros(num1,num2);
-Tx_test = zeros(num1,num2);
-yaw_error = zeros(num1,num2);
-yaw_accel_error = zeros(num1,num2);
+exit_flag = zeros(num1,num2, num3);
+Tx_test = zeros(num1,num2, num3);
+yaw_error = zeros(num1,num2, num3);
+yaw_accel_error = zeros(num1,num2, num3);
 P_all = zeros(num1, num2, num3);
 I_all = zeros(num1, num2, num3);
 T_all = zeros(num1, num2, num3);
@@ -39,9 +39,9 @@ for i = 1:num1
 
         P = s2;
         I = s3;
-        for k = 1:10
+        for k = 1
             P_all(i,j,k) = P;
-            I_all(i,j) = I;
+            I_all(i,j,k) = I;
             T_all(i,j,k) = T;
 
             simOut = sim('Vehicle_Model_Controller_Feb18','SimulationMode','normal',...
@@ -51,18 +51,18 @@ for i = 1:num1
 
             outputs = simOut.logsout;
             exit_flag_all = outputs.get('exit_flag').Values.Data;
-            exit_flag(i,j) = sum(0>exit_flag_all);
+            exit_flag(i,j,k) = sum(0>exit_flag_all);
 
             Tx_all = outputs.get('Tx').Values.Data;
-            Tx_test(i,j) = max(Tx_all(617:end,1)) - min(Tx_all(617:end,1));
+            Tx_test(i,j,k) = max(Tx_all(617:end,1)) - min(Tx_all(617:end,1));
 
             yaw_accel_error_all = outputs.get('Tracking Signal').Values.Data;
-            yaw_accel_error(i,j) = sqrt(mean((yaw_accel_error_all).^2));
+            yaw_accel_error(i,j,k) = sqrt(mean((yaw_accel_error_all).^2));
 
             yaw_accel_FS = max(yaw_accel_error_all(617:end)) - min(yaw_accel_error_all(617:end));
 
             yaw_error_all = outputs.get('yaw_error').Values.Data;
-            yaw_error(i,j) = sqrt(mean((yaw_error_all).^2));  
+            yaw_error(i,j,k) = sqrt(mean((yaw_error_all).^2));  
             toc
 
             test = 1;
