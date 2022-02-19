5fd6dbbc781de0f3843df6f6183733f388e3c5b7
invertedparadox <demetrius@users.noreply.github.com>
2022-02-19 16:30:00 -0500

    Instructions for PI control tuning

--- Controller Optimization/optimize_controller.m
+++ Controller Optimization/optimize_controller.m
@@ -1,19 +1,19 @@
 % Set Desired Parameters
 ts = 0.2:0.2:5;
-os = 0.5:0.5:30;
+os = 1:1:25;
+t_vary = 1:1:10;
 
 num1 = length(ts);
 num2 = length(os);
+num3 = length(t_vary);
 
 exit_flag = zeros(num1,num2);
 Tx_test = zeros(num1,num2);
 yaw_error = zeros(num1,num2);
 yaw_accel_error = zeros(num1,num2);
-P_all = zeros(num1, num2);
-I_all = zeros(num1, num2);
-
-% i_curr = i;
-% j_curr = j;
+P_all = zeros(num1, num2, num3);
+I_all = zeros(num1, num2, num3);
+T_all = zeros(num1, num2, num3);
 
 for i = 1:num1
     for j = 1:num2
@@ -37,34 +37,36 @@ for i = 1:num1
         s2 = 2 * Tx(3) * Tx(4);
         s3 = Tx(4)^2;
 
-%         P = s2;
-%         I = s3;
-        
-%         P_all(i,j) = P;
-%         I_all(i,j) = I;
-        
-        simOut = sim('Vehicle_Model_Controller_Feb18','SimulationMode','normal',...
-            'SaveState','on','StateSaveName','xout',...
-            'SaveOutput','on','OutputSaveName','yout',...
-            'SaveFormat', 'Dataset');
-        
-        outputs = simOut.logsout;
-        exit_flag_all = outputs.get('exit_flag').Values.Data;
-        exit_flag(i,j) = sum(0>exit_flag_all);
-        
-        Tx_all = outputs.get('Tx').Values.Data;
-        Tx_test(i,j) = max(Tx_all(617:end,1)) - min(Tx_all(617:end,1));
-        
-        yaw_accel_error_all = outputs.get('Tracking Signal').Values.Data;
-        yaw_accel_error(i,j) = sqrt(mean((yaw_accel_error_all).^2));
-        
-        yaw_accel_FS = max(yaw_accel_error_all(617:end)) - min(yaw_accel_error_all(617:end));
-        
-        yaw_error_all = outputs.get('yaw_error').Values.Data;
-        yaw_error(i,j) = sqrt(mean((yaw_error_all).^2));  
-        toc
-        
-        test = 1;
+        P = s2;
+        I = s3;
+        for k = 1:10
+            P_all(i,j,k) = P;
+            I_all(i,j) = I;
+            T_all(i,j,k) = T;
+
+            simOut = sim('Vehicle_Model_Controller_Feb18','SimulationMode','normal',...
+                'SaveState','on','StateSaveName','xout',...
+                'SaveOutput','on','OutputSaveName','yout',...
+                'SaveFormat', 'Dataset');
+
+            outputs = simOut.logsout;
+            exit_flag_all = outputs.get('exit_flag').Values.Data;
+            exit_flag(i,j) = sum(0>exit_flag_all);
+
+            Tx_all = outputs.get('Tx').Values.Data;
+            Tx_test(i,j) = max(Tx_all(617:end,1)) - min(Tx_all(617:end,1));
+
+            yaw_accel_error_all = outputs.get('Tracking Signal').Values.Data;
+            yaw_accel_error(i,j) = sqrt(mean((yaw_accel_error_all).^2));
+
+            yaw_accel_FS = max(yaw_accel_error_all(617:end)) - min(yaw_accel_error_all(617:end));
+
+            yaw_error_all = outputs.get('yaw_error').Values.Data;
+            yaw_error(i,j) = sqrt(mean((yaw_error_all).^2));  
+            toc
+
+            test = 1;
+        end
     end
 end
 
