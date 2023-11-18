1907aedaaf0c8aa97715dc8b431e08831b6e5c13
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2023-11-18 12:51:26 -0500

    code gen numba 2

---
+++ Controller Systems/controller_ert_rtw/ert_main.c
@@ -0,0 +1,118 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'controller'.
+ *
+ * Model version                  : 1.34
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Sat Nov 18 12:50:43 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#include <stddef.h>
+#include <stdio.h>            /* This example main program uses printf/fflush */
+#include "controller.h"                /* Model header file */
+
+static RT_MODEL rtM_;
+static RT_MODEL *const rtMPtr = &rtM_; /* Real-time model */
+static DW rtDW;                        /* Observable states */
+static PrevZCX rtPrevZCX;              /* Triggered events */
+static ExtU rtU;                       /* External inputs */
+static ExtY rtY;                       /* External outputs */
+
+/*
+ * Associating rt_OneStep with a real-time clock or interrupt service routine
+ * is what makes the generated code "real-time".  The function rt_OneStep is
+ * always associated with the base rate of the model.  Subrates are managed
+ * by the base rate from inside the generated code.  Enabling/disabling
+ * interrupts and floating point context switches are target specific.  This
+ * example code indicates where these should take place relative to executing
+ * the generated code step function.  Overrun behavior should be tailored to
+ * your application needs.  This example simply sets an error status in the
+ * real-time model and returns from rt_OneStep.
+ */
+void rt_OneStep(RT_MODEL *const rtM);
+void rt_OneStep(RT_MODEL *const rtM)
+{
+  static boolean_T OverrunFlag = false;
+
+  /* Disable interrupts here */
+
+  /* Check for overrun */
+  if (OverrunFlag) {
+    rtmSetErrorStatus(rtM, "Overrun");
+    return;
+  }
+
+  OverrunFlag = true;
+
+  /* Save FPU context here (if necessary) */
+  /* Re-enable timer or interrupt here */
+  /* Set model inputs here */
+
+  /* Step the model */
+  controller_step(rtM, &rtU, &rtY);
+
+  /* Get model outputs here */
+
+  /* Indicate task complete */
+  OverrunFlag = false;
+
+  /* Disable interrupts here */
+  /* Restore FPU context here (if necessary) */
+  /* Enable interrupts here */
+}
+
+/*
+ * The example main function illustrates what is required by your
+ * application code to initialize, execute, and terminate the generated code.
+ * Attaching rt_OneStep to a real-time clock is target specific. This example
+ * illustrates how you do this relative to initializing the model.
+ */
+int_T main(int_T argc, const char *argv[])
+{
+  RT_MODEL *const rtM = rtMPtr;
+
+  /* Unused arguments */
+  (void)(argc);
+  (void)(argv);
+
+  /* Pack model data into RTM */
+  rtM->dwork = &rtDW;
+  rtM->prevZCSigState = &rtPrevZCX;
+
+  /* Initialize model */
+  controller_initialize(rtM);
+
+  /* Attach rt_OneStep to a timer or interrupt service routine with
+   * period 0.01 seconds (base rate of the model) here.
+   * The call syntax for rt_OneStep is
+   *
+   *  rt_OneStep(rtM);
+   */
+  printf("Warning: The simulation will run forever. "
+         "Generated ERT main won't simulate model step behavior. "
+         "To change this behavior select the 'MAT-file logging' option.\n");
+  fflush((NULL));
+  while (rtmGetErrorStatus(rtM) == (NULL)) {
+    /*  Perform application tasks here */
+  }
+
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ Controller Systems/controller_ert_rtw/instrumented/controller.mk
@@ -0,0 +1,640 @@
+###########################################################################
+## Makefile generated for component 'controller'. 
+## 
+## Makefile     : controller.mk
+## Generated on : Sat Nov 18 12:50:54 2023
+## Final product: $(RELATIVE_PATH_TO_ANCHOR)/controller.elf
+## Product type : executable
+## 
+###########################################################################
+
+###########################################################################
+## MACROS
+###########################################################################
+
+# Macro Descriptions:
+# PRODUCT_NAME            Name of the system to build
+# MAKEFILE                Name of this makefile
+# COMPILER_COMMAND_FILE   Compiler command listing model reference header paths
+# CMD_FILE                Command file
+
+PRODUCT_NAME              = controller
+MAKEFILE                  = controller.mk
+MATLAB_ROOT               = C:/PROGRA~1/MATLAB/R2023b
+MATLAB_BIN                = C:/PROGRA~1/MATLAB/R2023b/bin
+MATLAB_ARCH_BIN           = $(MATLAB_BIN)/win64
+START_DIR                 = C:/Users/Inver/OneDrive/DOCUME~1/GitHub/SIMSCA~1/CONTRO~1
+SOLVER                    = 
+SOLVER_OBJ                = 
+CLASSIC_INTERFACE         = 0
+TGT_FCN_LIB               = ISO_C
+MODEL_HAS_DYNAMICALLY_LOADED_SFCNS = 0
+RELATIVE_PATH_TO_ANCHOR   = ../..
+COMPILER_COMMAND_FILE     = controller_comp.rsp
+CMD_FILE                  = controller.rsp
+C_STANDARD_OPTS           = 
+CPP_STANDARD_OPTS         = 
+
+###########################################################################
+## TOOLCHAIN SPECIFICATIONS
+###########################################################################
+
+# Toolchain Name:          GNU Tools for ARM Embedded Processors
+# Supported Version(s):    
+# ToolchainInfo Version:   2023b
+# Specification Revision:  1.0
+# 
+#-------------------------------------------
+# Macros assumed to be defined elsewhere
+#-------------------------------------------
+
+# TARGET_LOAD_CMD_ARGS
+# TARGET_LOAD_CMD
+# MW_GNU_ARM_TOOLS_PATH
+# FDATASECTIONS_FLG
+
+#-----------
+# MACROS
+#-----------
+
+LIBGCC                    = ${shell $(MW_GNU_ARM_TOOLS_PATH)/arm-none-eabi-gcc ${CFLAGS} -print-libgcc-file-name}
+LIBC                      = ${shell $(MW_GNU_ARM_TOOLS_PATH)/arm-none-eabi-gcc ${CFLAGS} -print-file-name=libc.a}
+LIBM                      = ${shell $(MW_GNU_ARM_TOOLS_PATH)/arm-none-eabi-gcc ${CFLAGS} -print-file-name=libm.a}
+PRODUCT_NAME_WITHOUT_EXTN = $(basename $(PRODUCT))
+PRODUCT_BIN               = $(PRODUCT_NAME_WITHOUT_EXTN).bin
+PRODUCT_HEX               = $(PRODUCT_NAME_WITHOUT_EXTN).hex
+CPFLAGS                   = -O binary
+SHELL                     = %SystemRoot%/system32/cmd.exe
+
+TOOLCHAIN_SRCS = 
+TOOLCHAIN_INCS = 
+TOOLCHAIN_LIBS = -lm
+
+#------------------------
+# BUILD TOOL COMMANDS
+#------------------------
+
+# Assembler: GNU ARM Assembler
+AS_PATH = $(MW_GNU_ARM_TOOLS_PATH)
+AS = "$(AS_PATH)/arm-none-eabi-gcc"
+
+# C Compiler: GNU ARM C Compiler
+CC_PATH = $(MW_GNU_ARM_TOOLS_PATH)
+CC = "$(CC_PATH)/arm-none-eabi-gcc"
+
+# Linker: GNU ARM Linker
+LD_PATH = $(MW_GNU_ARM_TOOLS_PATH)
+LD = "$(LD_PATH)/arm-none-eabi-g++"
+
+# C++ Compiler: GNU ARM C++ Compiler
+CPP_PATH = $(MW_GNU_ARM_TOOLS_PATH)
+CPP = "$(CPP_PATH)/arm-none-eabi-g++"
+
+# C++ Linker: GNU ARM C++ Linker
+CPP_LD_PATH = $(MW_GNU_ARM_TOOLS_PATH)
+CPP_LD = "$(CPP_LD_PATH)/arm-none-eabi-g++"
+
+# Archiver: GNU ARM Archiver
+AR_PATH = $(MW_GNU_ARM_TOOLS_PATH)
+AR = "$(AR_PATH)/arm-none-eabi-ar"
+
+# MEX Tool: MEX Tool
+MEX_PATH = $(MATLAB_ARCH_BIN)
+MEX = "$(MEX_PATH)/mex"
+
+# Binary Converter: Binary Converter
+OBJCOPYPATH = $(MW_GNU_ARM_TOOLS_PATH)
+OBJCOPY = "$(OBJCOPYPATH)/arm-none-eabi-objcopy"
+
+# Hex Converter: Hex Converter
+OBJCOPYPATH = $(MW_GNU_ARM_TOOLS_PATH)
+OBJCOPY = "$(OBJCOPYPATH)/arm-none-eabi-objcopy"
+
+# Executable Size: Executable Size
+EXESIZEPATH = $(MW_GNU_ARM_TOOLS_PATH)
+EXESIZE = "$(EXESIZEPATH)/arm-none-eabi-size"
+
+# Download: Download
+DOWNLOAD =
+
+# Execute: Execute
+EXECUTE = $(PRODUCT)
+
+# Builder: GMAKE Utility
+MAKE_PATH = %MATLAB%\bin\win64
+MAKE = "$(MAKE_PATH)/gmake"
+
+
+#-------------------------
+# Directives/Utilities
+#-------------------------
+
+ASDEBUG             = -g
+AS_OUTPUT_FLAG      = -o
+CDEBUG              = -g
+C_OUTPUT_FLAG       = -o
+LDDEBUG             = -g
+OUTPUT_FLAG         = -o
+CPPDEBUG            = -g
+CPP_OUTPUT_FLAG     = -o
+CPPLDDEBUG          = -g
+OUTPUT_FLAG         = -o
+ARDEBUG             =
+STATICLIB_OUTPUT_FLAG =
+MEX_DEBUG           = -g
+RM                  = @del /f/q
+ECHO                = @echo
+MV                  = @move
+RUN                 =
+
+#--------------------------------------
+# "Faster Runs" Build Configuration
+#--------------------------------------
+
+ARFLAGS              = ruvs
+ASFLAGS              = -MMD -MP -MF"$(@:%.o=%.dep)" -MT"$@"  \
+                       -Wall \
+                       -x assembler-with-cpp \
+                       $(ASFLAGS_ADDITIONAL) \
+                       $(DEFINES) \
+                       $(INCLUDES) \
+                       -c
+OBJCOPYFLAGS_BIN     = -O binary $(PRODUCT) $(PRODUCT_BIN)
+CFLAGS               = $(FDATASECTIONS_FLG) \
+                       -Wall \
+                       -MMD -MP -MF"$(@:%.o=%.dep)" -MT"$@"  \
+                       -c \
+                       -O3
+CPPFLAGS             = -std=gnu++14 \
+                       -fno-rtti \
+                       -fno-exceptions \
+                       $(FDATASECTIONS_FLG) \
+                       -Wall \
+                       -MMD -MP -MF"$(@:%.o=%.dep)" -MT"$@"  \
+                       -c \
+                       -O3
+CPP_LDFLAGS          = -Wl,--gc-sections \
+                       -Wl,-Map="$(PRODUCT_NAME).map"
+CPP_SHAREDLIB_LDFLAGS  =
+DOWNLOAD_FLAGS       =
+EXESIZE_FLAGS        = $(PRODUCT)
+EXECUTE_FLAGS        =
+OBJCOPYFLAGS_HEX     = -O ihex $(PRODUCT) $(PRODUCT_HEX)
+LDFLAGS              = -Wl,--gc-sections \
+                       -Wl,-Map="$(PRODUCT_NAME).map"
+MEX_CPPFLAGS         =
+MEX_CPPLDFLAGS       =
+MEX_CFLAGS           =
+MEX_LDFLAGS          =
+MAKE_FLAGS           = -f $(MAKEFILE)
+SHAREDLIB_LDFLAGS    =
+
+
+
+###########################################################################
+## OUTPUT INFO
+###########################################################################
+
+PRODUCT = $(RELATIVE_PATH_TO_ANCHOR)/controller.elf
+PRODUCT_TYPE = "executable"
+BUILD_TYPE = "Top-Level Standalone Executable"
+
+###########################################################################
+## INCLUDE PATHS
+###########################################################################
+
+INCLUDES_BUILDINFO = 
+
+INCLUDES = $(INCLUDES_BUILDINFO)
+
+###########################################################################
+## DEFINES
+###########################################################################
+
+DEFINES_BUILD_ARGS = -DCLASSIC_INTERFACE=0 -DALLOCATIONFCN=0 -DTERMFCN=0 -DONESTEPFCN=1 -DMAT_FILE=0 -DMULTI_INSTANCE_CODE=1 -DINTEGER_CODE=0 -DMT=0
+DEFINES_CUSTOM = 
+DEFINES_OPTS = -DTID01EQ=0
+DEFINES_STANDARD = -DMODEL=controller -DNUMST=2 -DNCSTATES=0 -DHAVESTDIO -DMODEL_HAS_DYNAMICALLY_LOADED_SFCNS=0
+
+DEFINES = $(DEFINES_BUILD_ARGS) $(DEFINES_CUSTOM) $(DEFINES_OPTS) $(DEFINES_STANDARD)
+
+###########################################################################
+## SOURCE FILES
+###########################################################################
+
+SRCS = $(START_DIR)/controller_ert_rtw/instrumented/controller.c $(START_DIR)/controller_ert_rtw/instrumented/controller_data.c coder_profile_timer.c
+
+MAIN_SRC = $(START_DIR)/controller_ert_rtw/instrumented/ert_main.c
+
+ALL_SRCS = $(SRCS) $(MAIN_SRC)
+
+###########################################################################
+## OBJECTS
+###########################################################################
+
+OBJS = controller.o controller_data.o coder_profile_timer.o
+
+MAIN_OBJ = ert_main.o
+
+ALL_OBJS = $(OBJS) $(MAIN_OBJ)
+
+###########################################################################
+## PREBUILT OBJECT FILES
+###########################################################################
+
+PREBUILT_OBJS = 
+
+###########################################################################
+## LIBRARIES
+###########################################################################
+
+LIBS = 
+
+###########################################################################
+## SYSTEM LIBRARIES
+###########################################################################
+
+SYSTEM_LIBS = 
+
+###########################################################################
+## ADDITIONAL TOOLCHAIN FLAGS
+###########################################################################
+
+#---------------
+# C Compiler
+#---------------
+
+CFLAGS_BASIC = $(DEFINES) $(INCLUDES) @$(COMPILER_COMMAND_FILE)
+
+CFLAGS += $(CFLAGS_BASIC)
+
+#-----------------
+# C++ Compiler
+#-----------------
+
+CPPFLAGS_BASIC = $(DEFINES) $(INCLUDES) @$(COMPILER_COMMAND_FILE)
+
+CPPFLAGS += $(CPPFLAGS_BASIC)
+
+#---------------------
+# MEX C++ Compiler
+#---------------------
+
+MEX_CPP_Compiler_BASIC =  @$(COMPILER_COMMAND_FILE)
+
+MEX_CPPFLAGS += $(MEX_CPP_Compiler_BASIC)
+
+#-----------------
+# MEX Compiler
+#-----------------
+
+MEX_Compiler_BASIC =  @$(COMPILER_COMMAND_FILE)
+
+MEX_CFLAGS += $(MEX_Compiler_BASIC)
+
+###########################################################################
+## INLINED COMMANDS
+###########################################################################
+
+
+ALL_DEPS:=$(patsubst %.o,%.dep,$(ALL_OBJS))
+all:
+
+ifndef DISABLE_GCC_FUNCTION_DATA_SECTIONS
+FDATASECTIONS_FLG := -ffunction-sections -fdata-sections
+endif
+
+
+
+-include codertarget_assembly_flags.mk
+-include ../codertarget_assembly_flags.mk
+-include ../../codertarget_assembly_flags.mk
+-include mw_gnu_arm_tools_path.mk
+-include ../mw_gnu_arm_tools_path.mk
+-include ../../mw_gnu_arm_tools_path.mk
+-include $(ALL_DEPS)
+
+
+###########################################################################
+## PHONY TARGETS
+###########################################################################
+
+.PHONY : all build buildobj clean info prebuild postbuild download execute
+
+
+all : build postbuild
+	@echo "### Successfully generated all binary outputs."
+
+
+build : prebuild $(PRODUCT)
+
+
+buildobj : prebuild $(OBJS) $(PREBUILT_OBJS)
+	@echo "### Successfully generated all binary outputs."
+
+
+prebuild : 
+
+
+postbuild : $(PRODUCT)
+	@echo "### Invoking postbuild tool "Binary Converter" ..."
+	$(OBJCOPY) $(OBJCOPYFLAGS_BIN)
+	@echo "### Done invoking postbuild tool."
+	@echo "### Invoking postbuild tool "Hex Converter" ..."
+	$(OBJCOPY) $(OBJCOPYFLAGS_HEX)
+	@echo "### Done invoking postbuild tool."
+	@echo "### Invoking postbuild tool "Executable Size" ..."
+	$(EXESIZE) $(EXESIZE_FLAGS)
+	@echo "### Done invoking postbuild tool."
+
+
+download : postbuild
+
+
+execute : download
+	@echo "### Invoking postbuild tool "Execute" ..."
+	$(EXECUTE) $(EXECUTE_FLAGS)
+	@echo "### Done invoking postbuild tool."
+
+
+###########################################################################
+## FINAL TARGET
+###########################################################################
+
+#-------------------------------------------
+# Create a standalone executable            
+#-------------------------------------------
+
+$(PRODUCT) : $(OBJS) $(PREBUILT_OBJS) $(MAIN_OBJ)
+	@echo "### Creating standalone executable "$(PRODUCT)" ..."
+	$(LD) $(LDFLAGS) -o $(PRODUCT) @$(CMD_FILE) $(SYSTEM_LIBS) $(TOOLCHAIN_LIBS)
+	@echo "### Created: $(PRODUCT)"
+
+
+###########################################################################
+## INTERMEDIATE TARGETS
+###########################################################################
+
+#---------------------
+# SOURCE-TO-OBJECT
+#---------------------
+
+%.o : %.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : %.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : %.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : %.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : %.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : %.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : %.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(RELATIVE_PATH_TO_ANCHOR)/%.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/%.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/rtw/c/src/%.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/simulink/src/%.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(MATLAB_ROOT)/toolbox/simulink/blocks/src/%.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.s
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.S
+	$(AS) $(ASFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.cpp
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.cc
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.C
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+%.o : $(START_DIR)/controller_ert_rtw/instrumented/%.cxx
+	$(CPP) $(CPPFLAGS) -o "$@" "$<"
+
+
+controller.o : $(START_DIR)/controller_ert_rtw/instrumented/controller.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+controller_data.o : $(START_DIR)/controller_ert_rtw/instrumented/controller_data.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+ert_main.o : $(START_DIR)/controller_ert_rtw/instrumented/ert_main.c
+	$(CC) $(CFLAGS) -o "$@" "$<"
+
+
+###########################################################################
+## DEPENDENCIES
+###########################################################################
+
+$(ALL_OBJS) : rtw_proj.tmw $(COMPILER_COMMAND_FILE) $(MAKEFILE)
+
+
+###########################################################################
+## MISCELLANEOUS TARGETS
+###########################################################################
+
+info : 
+	@echo "### PRODUCT = $(PRODUCT)"
+	@echo "### PRODUCT_TYPE = $(PRODUCT_TYPE)"
+	@echo "### BUILD_TYPE = $(BUILD_TYPE)"
+	@echo "### INCLUDES = $(INCLUDES)"
+	@echo "### DEFINES = $(DEFINES)"
+	@echo "### ALL_SRCS = $(ALL_SRCS)"
+	@echo "### ALL_OBJS = $(ALL_OBJS)"
+	@echo "### LIBS = $(LIBS)"
+	@echo "### MODELREF_LIBS = $(MODELREF_LIBS)"
+	@echo "### SYSTEM_LIBS = $(SYSTEM_LIBS)"
+	@echo "### TOOLCHAIN_LIBS = $(TOOLCHAIN_LIBS)"
+	@echo "### ASFLAGS = $(ASFLAGS)"
+	@echo "### CFLAGS = $(CFLAGS)"
+	@echo "### LDFLAGS = $(LDFLAGS)"
+	@echo "### SHAREDLIB_LDFLAGS = $(SHAREDLIB_LDFLAGS)"
+	@echo "### CPPFLAGS = $(CPPFLAGS)"
+	@echo "### CPP_LDFLAGS = $(CPP_LDFLAGS)"
+	@echo "### CPP_SHAREDLIB_LDFLAGS = $(CPP_SHAREDLIB_LDFLAGS)"
+	@echo "### ARFLAGS = $(ARFLAGS)"
+	@echo "### MEX_CFLAGS = $(MEX_CFLAGS)"
+	@echo "### MEX_CPPFLAGS = $(MEX_CPPFLAGS)"
+	@echo "### MEX_LDFLAGS = $(MEX_LDFLAGS)"
+	@echo "### MEX_CPPLDFLAGS = $(MEX_CPPLDFLAGS)"
+	@echo "### OBJCOPYFLAGS_BIN = $(OBJCOPYFLAGS_BIN)"
+	@echo "### OBJCOPYFLAGS_HEX = $(OBJCOPYFLAGS_HEX)"
+	@echo "### EXESIZE_FLAGS = $(EXESIZE_FLAGS)"
+	@echo "### DOWNLOAD_FLAGS = $(DOWNLOAD_FLAGS)"
+	@echo "### EXECUTE_FLAGS = $(EXECUTE_FLAGS)"
+	@echo "### MAKE_FLAGS = $(MAKE_FLAGS)"
+
+
+clean : 
+	$(ECHO) "### Deleting all derived files ..."
+	$(RM) $(subst /,\,$(PRODUCT))
+	$(RM) $(subst /,\,$(ALL_OBJS))
+	$(RM) *.dep
+	$(ECHO) "### Deleted all derived files."
+
+
