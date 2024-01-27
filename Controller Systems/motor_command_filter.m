960c6d1816161288460e3ab8c35c243cb1c0f248
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2024-01-27 13:50:35 -0500

    Update motor_command_filter.m

--- Controller Systems/motor_command_filter.m
+++ Controller Systems/motor_command_filter.m
@@ -1,4 +1,4 @@
-function [smoothed_command, FINISHED_SMOOTHING] = motor_command_filter(TVS_command_in, Equal_command_in, command_last_sent, TVS_PERMIT, LAST_TVS_PERMIT, FINISHED_SMOOTHING)
+function [smoothed_command, FINISHED_SMOOTHING] = motor_command_filter(TVS_command_in, Equal_command_in, TVS_PERMIT, LAST_TVS_PERMIT, command_last_sent, FINISHED_SMOOTHING)
 % Filters motor comands during TVS mode changes
 %   Detailed explanation goes here
 alpha = 0.1; % easing function coeffficient
