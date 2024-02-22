c5fc32b543d34c603a868acfd6b9b5bdd9c38ed3
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-02-21 21:58:18 -0500

    v1 tvs parameters

--- Controller Systems/load_tvs_MAX_SIMPLE.m
+++ Controller Systems/load_tvs_MAX_SIMPLE.m
@@ -1,63 +1,66 @@
-function tvs = load_tvs_MAX_SIMPLE()
-% skipped numbers: tvs.bounds.ub,tvs.bounds.lb,gain in outer Block Diagram
+function load_tvs_MAX_SIMPLE
 
-% Car Constants
-tvs.car.gear_ratio = 8.75;
-tvs.car.wheel_radius = 0.2286;
+% constants
+tvs.const.epsilon = 0.0001;
+tvs.const.s = [0.649 0.621]; % vehicle half track width for PER23 [front rear] (m)
+tvs.const.MOTOR_ENABLE = [0 0 1 1];
+tvs.const.valid_s = 3;
 
-% Estimation (need two layers!)
-tvs.estimation.nT = 149;
-tvs.estimation.nW = 10;
-tvs.estimation.nI = 20;
-tvs.estimation.nF = 6;
-tvs.estimation.valid_s = 3;
+% initial conditions
+tvs.ic.R = eye(3);
+tvs.ic.FINISHED_SMOOTHENING = 1;
+tvs.ic.LAST_TVS_PERMIT = 0;
+tvs.ic.k = [0 0];
 
-% Initial Conditions
-tvs.IC.R = eye(3);
-tvs.IC.LAST_TVS_PERMIT = 0;
-tvs.IC.FINISHED_SMOOTHENING = 1;
-tvs.IC.k = [0 0];
-tvs.IC.ID = zeros(25,1);
+% Tunable Parameters
+tvs.tune.P = 1;
+tvs.tune.r_power_sat = 0.5;
+tvs.tune.nI = 20;
+tvs.tune.nF = 6;
 
-% Range
-tvs.range.I = [-1 75]; % skipped
-tvs.range.K = [0 1]; % skipped
-tvs.range.Tm = [0 100]; % skipped
-tvs.range.Tmc = [0 75]; % skipped
+% Tables
+tvs.bp.Tm = [75 100]; % skipped
+tvs.bp.Tmc = [50 75]; % skipped
+tvs.bp.dIb = [0 5]; % skipped
+tvs.tbl.k_PL = [0 1]; % skipped
 
-tvs.bounds.ub = []; % skipped
-tvs.bounds.lb = []; % skipped
-
-% throttle map
 [v_sweep,w_sweep,minK_table] = create_minK_table();
 tvs.bp.V = v_sweep;
 tvs.bp.w = w_sweep;
-tvs.power_control.k_grid_min = minK_table;
+tvs.tbl.k_grid_min = minK_table;
 
-[~,~,maxK_table] = create_maxK_table(minK_table);
-tvs.power_control.maxK_table = maxK_table;
-tvs.power_control.dK_table = maxK_table - minK_table;
+% [~,~,maxK_table] = create_maxK_table(minK_table);
+% tvs.tbl.maxK_table = maxK_table;
+% tvs.tbl.dK_table = maxK_table - minK_table;
 
-% miscelaneous
-tvs.misc.epsilon = 0.0001;
+load yaw_table_2_21_24.mat
+tvs.bp.s = max_s_bp;
+tvs.bp.v = max_v_bp;
+tvs.tbl.yaw_table = yaw_table;
 
-% TVS Controller Parameters
-tvs.yaw_control.power_sat_const = 0.5;
-tvs.power_control.ABS_MAX_POWER_RATIO = [1 1 1 1];
-tvs.power_control.ABS_MAX_POWER_LEVEL = []; % skipped
-tvs.yaw_control.P = 1;
-tvs.yaw_control.geometry.s = []; % skipped
-tvs.power_control.MOTOR_ENABLE = [0 0 1 1];
+% Signal Ranges
+tvs.range.T = [0 4095];
+tvs.range.phi = [-130 130];
+tvs.range.V = [150 430];
+tvs.range.I = [0 125];
+tvs.range.w = [0 1100];
+tvs.range.xd = [-30 30];
+tvs.range.xdd = [-30 30];
+tvs.range.psid = [-2.5 2.5];
+tvs.range.tmc = [0 100];
+tvs.range.tm = [0 125];
+tvs.range.r = [0 1];
+tvs.range.PL = sum(tvs.const.MOTOR_ENABLE*tvs.range.r(2));
 
-% Yaw Tables
-tvs.yaw_control.yaw_table = [-flipud(tvs.yaw_control.max_yaw_table_ref(2:end,1:34)); tvs.yaw_control.max_yaw_table_ref(:,1:34)]';
-tvs.yaw_control.max_v_bp = tvs.yaw_control.max_v_bp(1:34);
-tvs.yaw_control.max_s_bp = [-fliplr(tvs.yaw_control.max_s_bp(2:end)) tvs.yaw_control.max_s_bp];
+tvs.range.lb = [tvs.range.T(1) tvs.range.phi(1) tvs.range.V(1) ... 
+    tvs.range.I(1) tvs.range.w(1) tvs.range.xd(1) tvs.range.xdd(1) ...
+    tvs.range.psid(1) tvs.range.tmc(1) tvs.range.tm(1)];
 
-% Power Tables
-tvs.bp.Tm = [75 100]; % skipped
-tvs.bp.Tmc = [50 75]; % skipped
-tvs.bp.Ib = [0 5]; % skipped
-tvs.power_control.K = [0 1]; % skipped
+tvs.range.ub = [tvs.range.T(2) tvs.range.phi(2) tvs.range.V(2) ... 
+    tvs.range.I(2) tvs.range.w(2) tvs.range.xd(2) tvs.range.xdd(2) ...
+    tvs.range.psid(2) tvs.range.tmc(2) tvs.range.tm(2)];
+
+% Save Parameters
+save("tvs_parameters.mat","tvs")
 
 end
