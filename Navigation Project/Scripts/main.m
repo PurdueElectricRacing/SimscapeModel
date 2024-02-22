31b2068980255d43bbd910ba4ef49f6c815f24dc
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-02-21 21:58:02 -0500

    beginning tvs testbench

---
+++ Navigation Project/Scripts/main.m
@@ -0,0 +1,34 @@
+%% Function Description
+% This function is the main function used for the wheel speed testbed.
+% 
+% Definitions
+% s - struct containing all data
+%
+% required functions: readData_c
+% Last Updated: 1/13/2024
+
+%% Parameter Identification
+% serialportlist - find available serial ports on your device
+
+%% The Code
+% Delete Previous Test
+clear s
+
+% Windows Serial Port
+s = serialport("COM3",115200);
+
+% Mac Serial Port
+% s = serialport("/dev/tty.usbmodem103",115200);
+
+% Erase Buffer
+flush(s);
+
+% Set the character that tells port that the message is finished
+configureTerminator(s,"CR");
+
+% Initialize data getting sent from microcontroller
+s.UserData = struct("Data",[0 0 0 0 0 0 0 0],"Count",1);
+
+% Start data transmission, provide function to run everytime data is sent
+% from microcontroller
+configureCallback(s,"terminator",@readData);
