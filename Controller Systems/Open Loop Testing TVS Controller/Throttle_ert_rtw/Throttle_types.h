6e76ff847f3ad19d86aa219213d5f38eeb5a40a5
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-03-02 11:08:26 -0500

    Throttle Map Code Generation

---
+++ Controller Systems/Open Loop Testing TVS Controller/Throttle_ert_rtw/Throttle_types.h
@@ -0,0 +1,34 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: Throttle_types.h
+ *
+ * Code generated for Simulink model 'Throttle'.
+ *
+ * Model version                  : 1.88
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Sat Mar  2 11:06:35 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex-M
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_Throttle_types_h_
+#define RTW_HEADER_Throttle_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM RT_MODEL;
+
+#endif                                 /* RTW_HEADER_Throttle_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
