132d2ced0082e942ca9430509d00564e41c1e6e1
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-04-12 22:56:54 -0400

    fixing engine map filtering

--- S1_VCU/C1_EM_FILTER/simulink_ac/ac_validation.m
+++ S1_VCU/C1_EM_FILTER/simulink_ac/ac_validation.m
@@ -1,3 +1,7 @@
+% at some point, the xyz axis for accelerometer and gyroscope were switched
+% this was fixed, and below is showing some testing data that validated
+% this
+
 % accel x&y are switched
 R = [0.814026 -0.001896 -0.580825; -0.001896 0.99998 -0.005922067; 0.580825 0.005922067 0.8140067];
 yaw = [1.34223223; 0.0611195; 2.1229372];
