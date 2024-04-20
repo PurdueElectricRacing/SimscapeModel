9a0e6e346ccce3b192dd6676b14677f5259dfe8c
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-04-20 10:04:52 -0400

    fix throttle map with no filter

--- S1_VCU/C1_EM_NO_FILTER/code_em/rtwtypes.h
+++ S1_VCU/C1_EM_NO_FILTER/code_em/rtwtypes.h
@@ -5,11 +5,11 @@
  *
  * File: rtwtypes.h
  *
- * Code generated for Simulink model 'tv'.
+ * Code generated for Simulink model 'em'.
  *
- * Model version                  : 1.31
+ * Model version                  : 1.40
  * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
- * C/C++ source code generated on : Sat Apr 20 09:30:06 2024
+ * C/C++ source code generated on : Sat Apr 20 09:58:48 2024
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex-M
