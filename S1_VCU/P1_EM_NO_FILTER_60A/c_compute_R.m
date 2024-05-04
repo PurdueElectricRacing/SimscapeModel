40dbec01acf4dcf8c6e4b00f246dd79fbd89101e
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-05-04 11:09:33 -0400

    add 60A variant

---
+++ S1_VCU/P1_EM_NO_FILTER_60A/c_compute_R.m
@@ -0,0 +1,25 @@
+%% Function Description
+% Given a single acceleration vector a_imu_xyz [3x1], 
+% this function computes the transformation matrix R that fulfills the
+% following relationship: R*a_imu_xyz = a_vehicle_xyz
+% This is done by finding R such that R*a_imu_xyz ~ [0;0;9.81] (roughly)
+% R is found using the axis-angle method
+
+%% The Function
+function R = c_compute_R(XYZs)
+    % define a_imu_xyz
+    XYZv = [0;0;sqrt(dot(XYZs,XYZs))];
+    
+    % Compute the angle between the two axis using dot product formula
+    theta = acos(dot(XYZs,XYZv)/(norm(XYZv)^2));
+    
+    % Find an axis that is orthogonal to both axis using cross product, normalize
+    u = cross(XYZs,XYZv);
+    u = u/norm(u);
+    
+    % Compute cross product matrix
+    ux = [0 -u(3) u(2); u(3) 0   -u(1); -u(2) u(1) 0];
+    
+    % Use Axis-Angle method to find the appropriate rotation matrix
+    R = (cos(theta)*eye(3)) + (sin(theta)*ux) + ((1-cos(theta))*(u*(u')));
+end
---
+++ S1_VCU/P1_EM_NO_FILTER_60A/create_yaw_table.m
@@ -0,0 +1,60 @@
+function [yaw_table, max_s_bp, max_v_bp] = create_yaw_table()
+
+%% Definitions
+% PCoGV - planar (x&y) center of gravity velocity
+% CCSA  - center column steering angle
+
+%% Import Data
+load("data/raw_yaw_data.mat");
+
+%% Parameters
+dv = 0.5; % m/s    velocity breakpoint for yaw map
+ds = 5; % deg      steering angle breakpoint for yaw map
+
+%% Simulation Conditions & Data
+steering_ref = ALL_SWEEP_DATA.ccw_steering(:,2);
+
+num2 = sum(steering_ref == 0) / 2;
+num3 = length(steering_ref) / num2;
+
+%% Extract Raw Data
+theta = Digital_Signals.Corner_Dynamics_Sensors.theta.Data;
+yaw_rates = Driver_signals.r.Data;
+xdot = Driver_signals.Vel.xdot.Data;
+ydot = Driver_signals.Vel.ydot.Data;
+
+%% Process Raw Data into Clean Data
+end_positions = [find(counter == 0); length(counter)];  % indicies for the end of each PCoGV & CCSA setpoint
+velocities = sqrt(xdot.^2 + ydot.^2);      % vehicle PCoGV (m/s)
+
+num_data_not_collected = num3*num2 - length(end_positions);
+yaw_rate_vector = [yaw_rates(end_positions); zeros(num_data_not_collected,1)];
+
+yaw_rate = reshape(yaw_rate_vector, [num3 num2]);
+yaw_rate = [yaw_rate zeros(28,1) zeros(28,1)];
+
+velocity = reshape(velocities(end_positions), [num3 num2]);
+velocity = [velocity zeros(28,1) 25*ones(28,1)];
+
+steering = reshape(theta(end_positions), [num3 num2]);
+steering = [steering steering(:,1) steering(:,1)];
+
+%% Calculate All RAW Lookup Tables
+limit_velocity = velocity(1,:);
+limit_steering = steering(:,1);
+
+max_v_bp = min(limit_velocity):dv:max(limit_velocity);
+max_s_bp = min(limit_steering):ds:max(limit_steering);
+[limit_velocity_grid, limit_steering_grid] = meshgrid(max_v_bp,max_s_bp);
+
+% calculate overall lookup table
+[xData, yData, zData] = prepareSurfaceData( velocity, steering, yaw_rate );
+ft = 'linearinterp';
+[fitresult3, gof] = fit( [xData, yData], zData, ft, 'Normalize', 'on' );
+
+yaw_table = feval(fitresult3, limit_velocity_grid,limit_steering_grid);
+
+% the final table
+yaw_table = [-flipud(yaw_table(2:end,:)); yaw_table]';
+max_s_bp = [-fliplr(max_s_bp(2:end)) max_s_bp];
+end
