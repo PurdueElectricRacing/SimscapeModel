06cf066297c398d1c2958d2a8d310b318e0046d5
DEMETRIUS GULEWICZ <invertedparadox@outlook.com>
2024-06-22 17:33:07 -0400

    update to AMK

--- T1_Pysical_Testbench/AMK Tables/compute_AMK.m
+++ T1_Pysical_Testbench/AMK Tables/compute_AMK.m
@@ -7,7 +7,8 @@ load A2370DD_T80C.mat
 speed_matrix = Speed.*(pi/30); % speed breakpoints (rad/s)
 current_matrix = repmat(0:5.25:105,[m,1]); % current matrix (A)
 torque_matrix = Shaft_Torque; % Output motor torque (Nm)
-voltageDC_matrix = Voltage_Phase_RMS.*sqrt(2); % DC voltage
+voltageDC_matrix = Voltage_Phase_RMS.*sqrt(2); % DC voltage (V)
+loss_matrix = Total_Loss; % power wasted (W)
 
 % speed breakpoint definition
 min_speed = 0;
@@ -38,15 +39,16 @@ speed_matrix = speed_matrix(2:end,:);
 current_matrix = current_matrix(2:end,:);
 torque_matrix = torque_matrix(2:end,:);
 voltageDC_matrix = voltageDC_matrix(2:end,:);
+loss_matrix = loss_matrix(2:end,:);
 
 %% calculate max and min motor torque
 [minT_tbl, maxT_tbl] = compute_barrierT_tbl(speed_matrix,torque_matrix,voltageDC_matrix,current_matrix,voltageT_tbl,speed_bp,nv,ns);
 
-%% calculate battery current draw
-motorI_tbl = compute_motorI_tbl(speed_matrix,torque_matrix,current_matrix,speedI_tbl,torqueI_tbl);
+%% calculate inverter current draw
+inverterI_tbl = compute_inverterI_tbl(speed_matrix,torque_matrix,loss_matrix,speedI_tbl,torqueI_tbl);
 
 %% Save and Clear
-clearvars -except speedT_tbl voltageT_tbl minT_tbl maxT_tbl speedI_tbl torqueI_tbl motorI_tbl
+clearvars -except speedT_tbl voltageT_tbl minT_tbl maxT_tbl speedI_tbl torqueI_tbl inverterI_tbl
 save("AMK_lookup")
 
 %% visualize 
@@ -57,7 +59,7 @@ minT_tbl_flat = minT_tbl(:);
 maxT_tbl_flat = maxT_tbl(:);
 speedI_tbl_flat = speedI_tbl(:);
 torqueI_tbl_flat = torqueI_tbl(:);
-motorI_tbl_flat = motorI_tbl(:);
+inverterI_tbl_flat = inverterI_tbl(:);
 
 % max and min torque tables
 figure(1);
@@ -70,10 +72,10 @@ ylabel("Supply Voltage (V)")
 zlabel("Torque (Nm)")
 legend("Min", "Max")
 
-% motor DC current table
+% inverter DC current table
 figure(2);
-scatter3(speedI_tbl_flat,torqueI_tbl_flat,motorI_tbl_flat)
+scatter3(speedI_tbl_flat,torqueI_tbl_flat,inverterI_tbl_flat./1000)
 
 xlabel("Motor Speed (rad/s)")
 ylabel("Torque (Nm)")
-zlabel("Motor Current (A)")
+zlabel("Inverter Power (kW)")
--- T1_Pysical_Testbench/AMK Tables/compute_inverterI_tbl.m
+++ T1_Pysical_Testbench/AMK Tables/compute_inverterI_tbl.m
@@ -1,9 +1,9 @@
-function motorI_tbl = compute_motorI_tbl(speed_matrix,torque_matrix,current_matrix,speedI_tbl,torqueI_tbl)
+function inverterI_tbl = compute_inverterI_tbl(speed_matrix,torque_matrix,loss_matrix,speedI_tbl,torqueI_tbl)
     %% Step 1: form overall raw table
     minT_matrix = -torque_matrix(:,2:end) + 2*torque_matrix(:,1);
     torque_matrix_all = [torque_matrix  minT_matrix];
     speed_matrix_all = [speed_matrix speed_matrix(:,2:end)];
-    current_matrix_all = [current_matrix -current_matrix(:,2:end)];
+    power_matrix_all = torque_matrix_all.*speed_matrix_all + [loss_matrix loss_matrix(:,2:end)];
 
     %% Step 2: make torque-speed-current a bijective function
     not_bijective_matrix_max = abs(torque_matrix(:,1:end-1) - torque_matrix(:,2:end)) < 0.5;
@@ -12,15 +12,15 @@ function motorI_tbl = compute_motorI_tbl(speed_matrix,torque_matrix,current_matr
     
     speed_matrix_bijectiveT = speed_matrix_all(~not_bijective_matrix);
     torque_matrix_bijectiveT = torque_matrix_all(~not_bijective_matrix);
-    current_matrix_bijectiveT = current_matrix_all(~not_bijective_matrix);
+    power_matrix_bijectiveT = power_matrix_all(~not_bijective_matrix);
 
     %% Step 3: make full torque-speed-current table   
-    [xData, yData, zData] = prepareSurfaceData(speed_matrix_bijectiveT,torque_matrix_bijectiveT,current_matrix_bijectiveT);
+    [xData, yData, zData] = prepareSurfaceData(speed_matrix_bijectiveT,torque_matrix_bijectiveT,power_matrix_bijectiveT);
     ft = 'cubicinterp';
     opts = fitoptions('Method','CubicSplineInterpolant');
     opts.ExtrapolationMethod = 'linear';
     opts.Normalize = 'on';
-    current_all_fit = fit([xData,yData],zData,ft,opts);
+    power_all_fit = fit([xData,yData],zData,ft,opts);
     
-    motorI_tbl = feval(current_all_fit, speedI_tbl, torqueI_tbl);
+    inverterI_tbl = feval(power_all_fit, speedI_tbl, torqueI_tbl);
 end
