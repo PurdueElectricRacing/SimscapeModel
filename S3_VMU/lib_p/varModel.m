25e6249e68f90d275bd468879e425817fc8b39ab
DEMETRIUS GULEWICZ <dgulewic@purdue.edu>
2024-08-31 13:55:32 -0400

    update varModel

--- S3_VMU/lib_p/varModel.m
+++ S3_VMU/lib_p/varModel.m
@@ -8,7 +8,7 @@ classdef varModel < handle
         O0;  % orientation of the vehicle body at 0 force [rad]
         r0;  % unloaded tire radius [front rear] [m]
         k;   % suspension spring constant [front rear] [N/m]
-        k1;  % Tire spring constant [front rear] [m/N]
+        k0;  % Tire spring constant [front rear] [m/N]
         c;   % damping constant [front rear] [Ns/m]
         wb;  % vehicle wheelbase [front rear] [m]
         ht;  % vehicle half track [front rear] [m]
@@ -23,6 +23,8 @@ classdef varModel < handle
         ir;  % Internal resistance of cell [Ohm]
         cr;  % Capacitance of voltage regulating capacitor
 
+
+        
 
         d;  % coefficient of friction model coefficients
         ct; % lookup table for damper coefficients [m/s] -> [Ns/m]
@@ -40,22 +42,26 @@ classdef varModel < handle
             % Exact Constants
             varVehicle.m = 219 + 68;
             varVehicle.g = 9.81;
-            varVehicle.r = 0.2;
+            varVehicle.r0 = 0.2;
             varVehicle.k = 43780*2*[1;1];
             varVehicle.c = ppval(varVehicle.ct, [0; 0]);
             varVehicle.wb = 1.535*[1-0.46; 0.46];
             varVehicle.ht = [1.34; 1.27];
-            varVehicle.cl = 2.11014;
-            varVehicle.cd = 1.149;
+            varVehicle.cl = 0.5*2.11014*1.225*0.1;
+            varVehicle.cd = 0.5*1.149*1.225*0.1;
             varVehicle.Jv = 200;
             varVehicle.Jw = 0.3;
             varVehicle.gr = 8.75;
             varVehicle.xp = 0.1*varVehicle.wb(1);
+<<<<<<< Updated upstream
             varVehicle.ns = 150;
             varVehicle.np = 5;
             varVehicle.vt = get_v_table();
             varVehicle.ir = 0.0144;
             varVehicle.cr = 0.006;
+=======
+            varVehicle.k0 = 5.0000e-06;
+>>>>>>> Stashed changes
 
             % Dependent Parameters
             varVehicle.zs = 0.182;
