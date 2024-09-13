5413982b1dd2c999c4f7c9302bb35797556e8503
arclnx <73797060+arclnx@users.noreply.github.com>
2024-09-12 20:19:33 -0400

    ASDf

--- S3_VMU/lib_p/No_Slip_Model/main_no_slip.m
+++ S3_VMU/lib_p/No_Slip_Model/main_no_slip.m
@@ -1,11 +1,3 @@
-%% Add master model to path
-% addpath("..\Master_Model","-end")
-% addpath("..\Damper_Tables","-end")
-% addpath("..\Master_Model","-end")
-% addpath("..\Battery_Tables","-end")
-% addpath("..\Motor_Tables","-end")
-% addpath("..\Tire_Tables","-end")
-
 %% Get Model
 varCAR = varModel_master;
 
