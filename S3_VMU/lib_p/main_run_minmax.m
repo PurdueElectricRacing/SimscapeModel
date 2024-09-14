71a5188cf355a78d3deddfbcb33785cf4b1648c0
arclnx <73797060+arclnx@users.noreply.github.com>
2024-09-14 13:38:05 -0400

    Update main_run_minmax.m

--- S3_VMU/lib_p/main_run_minmax.m
+++ S3_VMU/lib_p/main_run_minmax.m
@@ -2,14 +2,14 @@
 addpath(genpath(pwd))
 
 data_table = table();
-data_table.models = ["Master"; "No Slip"];
+data_table.model = ["Master"; "No Slip"];
 
 %% Master Model
 % run model
 main_master
 
 % plot data
-plot_master(v_master, data_table.models(1));
+plot_master(v_master, data_table.model(1));
 
 data_table.accel_time(1) = t(find(s(:,2)>75, 1));
 data_table.charge_used(1) = s(1,11) - s(end,11);
@@ -19,7 +19,7 @@ data_table.charge_used(1) = s(1,11) - s(end,11);
 main_no_slip
 
 % plot data
-plot_master(v_no_slip, data_table.models(2));
+plot_master(v_no_slip, data_table.model(2));
 
 data_table.accel_time(2) = t(find(s(:,2)>75, 1));
 data_table.charge_used(2) = s(1,11) - s(end,11);
