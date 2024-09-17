5d893a301a86c02ab75f246e165ffe60b388952e
arclnx <73797060+arclnx@users.noreply.github.com>
2024-09-17 15:46:43 -0400

    working on realistic controller

---
+++ S3_VMU/lib_p/Basic_Real_Model/main_basic_real.m
@@ -0,0 +1,17 @@
+%% Get Model
+varCAR = varModel_master;
+
+%% Initial Conditions
+s = [0.1; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; varCAR.v0; 0];
+
+%% Boundary Conditions
+tau = [0; 25];
+
+%% Configure Solver
+optionsODE = odeset('MaxStep',0.01);
+
+%% Simulate
+[t,s] = ode23tb(@compute_ds_basic_real, [0 70], s, optionsODE, tau, varCAR);
+
+%% Pack output
+v_basic_real = compute_v_basic_real(t, s, tau, varCAR);
