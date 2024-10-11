6d5e8fbdd572564659e6be0e1030035341d16167
WuWilliam0713 <wuwilliam.1@gmail.com>
2024-10-10 21:02:50 -0400

    Update try_to_be_straight.m

--- S2_VDU/try_to_be_straight.m
+++ S2_VDU/try_to_be_straight.m
@@ -1,4 +1,5 @@
-load Tracks\Tracks_Mat\acceleration.mat  % Direction(
+load Tracks\Tracks_Mat\acceleration.mat
+% [Dr(slop) Ypos Xpos TrngRad TrngDr(L/R) InstCY InstCX psi]
 
 strang = -180 + 360 * rand(1,100);  % Steering angle in Degrees
 thrtl = 1 * rand(1,100);            % Throttle depth 0 ~ 1
