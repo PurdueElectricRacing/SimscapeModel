f532209a399d8ccecc2476c280ee4087b1c376b1
WuWilliam0713 <wuwilliam.1@gmail.com>
2024-10-10 20:58:34 -0400

    randomly generate steering angle and thrtl

---
+++ S2_VDU/try_to_be_straight.m
@@ -0,0 +1,7 @@
+load Tracks\Tracks_Mat\acceleration.mat  % Direction(
+
+strang = -180 + 360 * rand(1,100);  % Steering angle in Degrees
+thrtl = 1 * rand(1,100);            % Throttle depth 0 ~ 1
+
+
+
