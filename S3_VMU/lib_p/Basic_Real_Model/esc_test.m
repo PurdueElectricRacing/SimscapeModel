c5bec57249c91793bfcf0be267ca4d42aa6c5f37
arclnx <73797060+arclnx@users.noreply.github.com>
2024-11-02 12:17:56 -0400

    deleted asv

--- S3_VMU/lib_p/Basic_Real_Model/esc_test.m
+++ S3_VMU/lib_p/Basic_Real_Model/esc_test.m
@@ -16,14 +16,14 @@ ans=1;
 
 
 dt = 0.1;
-a = 0.25;
-b = 0.25;
-w = 4;
+a = 1;
+b = 0.05;
+w = 1;
 p = 1;
 k = 1;
 
 
-t_All = (0:dt:50)';
+t_All = (0:dt:5000)';
 %Y_all = sin(0.5.*t_All) + 1;
 Y_all = zeros(size(t_All));
 H_all = zeros(size(t_All));
@@ -34,22 +34,26 @@ Theta_all = zeros(size(t_All));
 for i = 2:length(t_All)-1
     H_all(i+1) = H_Y(H_all(i), p, dt, Y_all(i), Y_all(i-1));
     X_all(i+1) = X_H(H_all(i+1), a, w, t_All(i+1));
-    W_all(i+1,:) = Thetahat_X(W_all(i,:)', H_all(i), p, dt, Y_all(i), Y_all(i-1), a, w, t_All(i), k)';
-    Theta_all(i+1) = Theta_Thetahat(W_all(i+1, 2), b, w, t_All(i));
+    W_all(i+1,:) = Thetahat_X(W_all(i,:)', H_all(i), p, dt, Y_all(i), Y_all(i-1), a, w, t_All(i+1), k)';
+    Theta_all(i+1) = Theta_Thetahat(W_all(i+1, 2), b, w, t_All(i+1));
     Y_all(i+1) = Y_Theta(Theta_all(i+1));
 end
 
+figure(1)
 plot(t_All, Y_all); hold on
 plot(t_All, H_all)
 plot(t_All, X_all)
-plot(t_All, W_all(:,1))
+plot(t_All, W_all(:,2))
 plot(t_All, Theta_all)
 plot(t_All, Y_all)
 legend("Y", "H", "X", "Thetahat", "Theta", "Y")
+figure(2)
+plot(t_All, Y_all); hold on
+plot(t_All, W_all(:,2))
 
 %% Functions
 function [Y] = Y_Theta(theta)
-    Y = 10*theta - 0.1*theta^2;
+    Y = theta - theta^2;
 end
 
 function [H_kp1] = H_Y(H_k, p, dt, Y_k, Y_km1)
@@ -68,7 +72,7 @@ end
 function [W_kp1] = Thetahat_X(W_k, H_k, p, dt, Y_k, Y_km1, a, w, t, k)
    expAx = @(x) ([exp(-p*x), 0; (1-exp(-p*x))/p, 1]); % exp(A*x)
    d = @(t) ([k * interp_X(H_k,p,dt,Y_k,Y_km1,a,w,t); 0]);
-   W_kp1 = expAx(dt) * W_k + integral(@(tau) (expAx(tau)*d(tau+t)), 0, dt,ArrayValued=true);
+   W_kp1 = expAx(dt) * W_k + integral(@(tau) (expAx(tau)*d(tau+t)), 0, dt, ArrayValued=true);
 end
 
 function [theta] = Theta_Thetahat(Thetahat, b, w, t)
