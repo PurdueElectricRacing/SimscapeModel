aa56646057895159d5793d6cc18677cb55016779
arclnx <73797060+arclnx@users.noreply.github.com>
2024-11-05 17:27:13 -0500

    converted simulation into a function

--- S3_VMU/lib_p/Basic_Real_Model/esc_simulation.m
+++ S3_VMU/lib_p/Basic_Real_Model/esc_simulation.m
@@ -13,8 +13,8 @@ eA = expm(A);
 g = eA*[k; 0]*xt;
 f = int(g, dt, 0, dt);
 
-wt = eA*w0 + f;
-func = matlabFunction(wt);
+Wt = eA*w0 + f;
+func = matlabFunction(Wt);
 
 clearvars -except func
 
@@ -25,39 +25,21 @@ k = 0.7;
 w = 10;
 ph = 1;
 pl = 1;
-dt = 0.0001;
+dt = 0.01;
 
-w1 = 0;
-w2 = 0;
-y0 = 0;
+W1 = 0;
+W2 = 1;
+y0 = 3;
 du0 = 0;
 t0 = 0;
+tf=50;
 
-%% Simulate
-t_vec = 0:dt:150;
-N = length(t_vec) - 1;
-wt = zeros(N+1,2);
-theta = zeros(N+1,1);
-
-for i = 2:N+1
-    % compute next step
-    wt(i,:) = func(a,dt,du0,k,ph,pl,t0,w,w1,w2,y0);
-    theta(i) = wt(i,2) + b*sin(w*t_vec(i));
-
-    % update inputs
-    y0 = get_y0(theta(i));
-    w1 = wt(i,1);
-    w2 = wt(i,2);
-    du0 = y0 - get_y0(theta(i-1));
-    t0 = t_vec(i);
-
-    % get estimate
-    y0_hat = wt(i,2)-wt(i,2)^2;
-end
+% simulate
+[t_vec, theta, theta_hat, y] = simulate(a, b, k, w, ph, pl, dt, W1, W2, y0, du0, t0, tf, func);
 
 %% Plot
 figure(1)
-plot(t_vec, wt(:,2))
+plot(t_vec, theta_hat)
 ylabel("theta hat")
 
 figure(2)
@@ -65,14 +47,42 @@ plot(t_vec, theta)
 ylabel("perturbed estimate")
 
 figure(3)
-plot(t_vec, get_y0(theta))
-ylabel("estimated y")
+plot(t_vec, y)
+ylabel("plant output (y)")
 
 %% Functions
 % cost function
 % cost is combination of average error, 
 function [cost] = cost(a, b, k, w, ph, pl)
     
+end
+
+% simulate
+function [t, theta, theta_hat, y] = simulate(a, b, k, w, ph, pl, dt, W1, W2, y0, du0, t0, tf, func)
+    %setupt
+    t_vec = 0:dt:tf;
+    N = length(t_vec) - 1;
+    Wt = zeros(N+1,2); Wt(1,:) = [W1 W2];
+    theta = zeros(N+1,1); theta(1) = Wt(1,2) + b*sin(w*t_vec(1));
+    
+    % simulate
+    for i = 2:N+1
+        % compute next step
+        Wt(i,:) = func(a,dt,du0,k,ph,pl,t0,w,W1,W2,y0);
+        theta(i) = Wt(i,2) + b*sin(w*t_vec(i));
+    
+        % update inputs
+        y0 = get_y0(theta(i));
+        W1 = Wt(i,1);
+        W2 = Wt(i,2);
+        du0 = y0 - get_y0(theta(i-1));
+        t0 = t_vec(i);
+    end
+
+    % outputs
+    t = t_vec;
+    theta_hat = Wt(:,2);
+    y = get_y0(theta);
 end
 
 % plant
