797b2e95f09cb8626e337b566965940ff3bf75fb
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2024-11-09 12:44:09 -0500

    add timing

--- S3_VMU/lib_p/main_run_minmax.m
+++ S3_VMU/lib_p/main_run_minmax.m
@@ -6,7 +6,9 @@ data_table.model = ["Master"; "No Slip"; "Basic Real"];
 
 %% Master Model
 % run model
+t0 = tic;
 main_master
+t1 = toc(t0);
 
 % plot data
 plot_master(v_master, data_table.model(1));
@@ -15,9 +17,10 @@ data_table.accel_time(1) = t(find(s(:,2)>75, 1));
 data_table.charge_used(1) = s(end,11) - s(1,11);
 
 %% No Slip Model
-
 % run model
+t0 = tic;
 main_no_slip
+t2 = toc(t0);
 
 % plot data
 plot_master(v_no_slip, data_table.model(2));
@@ -26,9 +29,10 @@ data_table.accel_time(2) = t(find(s(:,2)>75, 1));
 data_table.charge_used(2) = s(end,11) - s(1,11);
 
 %% Basic Real Model
-
 % run model
+t0 = tic;
 main_basic_real
+t3 = toc(t0);
 
 % plot data
 plot_master(v_basic_real, data_table.model(3))
