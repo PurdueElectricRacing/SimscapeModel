a555cf2d5c87f132fa0382f702194e4300bda9e6
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2024-11-23 12:20:53 -0500

    controller struct

---
+++ S1_VCU/TVS_25/varController_Master_TVS.m
@@ -0,0 +1,17 @@
+classdef varController_Master_TVS < handle
+    %% Controller Properties
+    properties
+        p; % some property
+
+    end
+
+    %% Controller Methods
+    methods
+        function varController = varController_Master_TVS()
+            varController.p = 1; % assignment of property
+
+        end
+
+    end
+
+end
