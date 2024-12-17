5c09b249f3f68496ff0a55d21d90c71531aad93d
DemetriusGulewicz <dgulewic@purdue.edu>
2024-12-17 13:11:27 -0500

    fixing notation

--- S3_VMU/lib_p/Master_Model_3DOF/compute_ds_master_3DOF.m
+++ S3_VMU/lib_p/Master_Model_3DOF/compute_ds_master_3DOF.m
@@ -11,7 +11,7 @@
 %  s(8)  = wr  [rad/s] - the differential angular velocity of the rear tires
 %  s(9)  = Voc [V] - the open circuit voltage of the HV battery
 %  s(10) = Vb  [V] - the voltage across the terminals of the HV battery
-%  s(11) = Ah  [A*hr] - the charge drained from the HV battery, 0 corresponds to full charge
+%  s(11) = As  [A*s] - the charge drained from the HV battery, 0 corresponds to full charge
 %  s(12) = Imf [A] - the current pulled by the front powertrain
 %  s(13) = Imr [A] - the current pulled by the rear powertrain
 
@@ -19,7 +19,7 @@
 function ds = compute_ds_master_3DOF(t, s, tauRaw, varCAR)
     [Fx, Fz, wt, tau] = traction_model_master_3DOF(s, varCAR);
     [ddx, ddz, ddo, dw] = vehicle_dynamics_model_master_3DOF(s, Fx, Fz, wt, tau, varCAR);
-    [dVoc, dVb, dAh, dIm] = powertrain_model_master_3DOF(s, wt, tauRaw, varCAR);
+    [dVoc, dVb, dAs, dIm] = powertrain_model_master_3DOF(s, wt, tauRaw, varCAR);
 
-    ds = [ddx; s(1); ddz; s(3); ddo; s(5); dw; dVoc; dVb; dAh; dIm];
+    ds = [ddx; s(1); ddz; s(3); ddo; s(5); dw; dVoc; dVb; dAs; dIm];
 end
