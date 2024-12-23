2d08b01521aa429dfe694106ca67fd255d01e018
PrakharDrolia <droliaprakhar15@outlook.com>
2024-12-23 11:25:07 -0500

    Simulink to MATLAB reference
    Not final. Does not reference other files, loads the matfile. Pushing to save, show and share. Variable names need to be cleaned and aligned to final requirements. Need to add validations and bounds for range of input values. Fix lookup table recall. Fix the saturation translations.

---
+++ S1_VCU/TVS_25/TV_transl_simulink_Ref.m
@@ -0,0 +1,71 @@
+% A direct translation of the simulink model:
+% Torque_Vectoring_2WD_NO_FILTER_NO_REGEN
+
+var = load("tvs_vars.mat");
+
+inp = [50, 50, 50, 50];
+
+m_temp_max = [-50; 130];
+mc_temp_max = [-50; 130];
+bt_temp_max = [-50; 65];
+bI_current = [-1; 160];
+
+sys_bias = [var.mT_bias, var.mcT_bias, var.bT_bias, var.bI_bias];
+sys_gain = [var.mT_gain, var.mcT_gain, var.bT_gain, var.bI_gain];
+add_gain = [1,1,1,1];
+
+r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var,1, 0, [1, 1, 1], 15, 120, 13, 1, 1)
+
+function r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var, r_EQUAL, lo_constant, ang_vel,vel_gs, phi, dphi, TVS_I, TVS_P)
+
+    inp = [inp(1)+sys_bias(1), inp(2)+sys_bias(2), inp(3)+sys_bias(3), inp(4)+sys_bias(4)];
+    gained_inp = inp.*sys_gain;
+    gained_bias = [gained_inp(1)+add_gain(1), gained_inp(2)+add_gain(2), gained_inp(3)+add_gain(3), gained_inp(4)+add_gain(4)];
+    min_out = min(gained_bias);
+
+    min_saturation_out = min(max(min_out, var.rb(1)), var.rb(2))
+
+    arrcheck = [r_EQUAL, lo_constant, min_saturation_out];
+    powerlimit = max(arrcheck);
+    
+
+    if -130<phi<130
+        phi_sat = phi;
+    elseif -130>phi
+        phi_sat = -130;
+    else
+        phi_sat = 130;
+    end
+
+    yaw_lookup = var.yaw_table(vel_gs, 3);
+
+    ref_yaw = yaw_lookup.*TVS_I;
+
+    ang_z = ang_vel(3);
+
+    yaw_error = (-ang_z)+ref_yaw;
+    
+    error_prod = yaw_error.*TVS_P;
+     
+    raw_dR = error_prod.*var.half_track;
+
+    up = powerlimit.*var.r_power_sat;
+
+    lo = up.*-1;  
+
+    arrcheck = [up, raw_dR, lo];
+
+    e = max(arrcheck);
+
+    steering = abs(phi_sat) > dphi;
+
+    function_inp_u = e.*steering;
+
+    if function_inp_u > 0
+        r_TVS = [powerlimit powerlimit-abs(function_inp_u)];
+    else
+        r_TVS = [powerlimit-abs(function_inp_u) powerlimit];
+    end
+end
+                
+
