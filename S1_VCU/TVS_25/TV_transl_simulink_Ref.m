749b7155790bc44b0bd11afafbf80d266d8fd24d
PrakharDrolia <droliaprakhar15@outlook.com>
2025-01-16 17:11:37 -0500

    Safety with Cleaning
    Cleaned some length of code. Logical and parameter updates still to be made.

--- S1_VCU/TVS_25/TV_transl_simulink_Ref.m
+++ S1_VCU/TVS_25/TV_transl_simulink_Ref.m
@@ -4,8 +4,7 @@
 % please fill in varController_Master_TVS.m with parameters
 var = load("tvs_vars.mat");
 
-% inp is not a super descriptive name mT_mcT_bT_bI
-inp = [50, 50, 50, 50];
+inp = [50, 50, 50, 50];%mT_mcT_bT_bI =  %inp currently is initialised still even though final will be the mT... to still be able to test logic in program
 
 m_temp_max = [-50; 130];
 mc_temp_max = [-50; 130];
@@ -17,19 +16,18 @@ sys_bias = [var.mT_bias, var.mcT_bias, var.bT_bias, var.bI_bias];
 sys_gain = [var.mT_gain, var.mcT_gain, var.bT_gain, var.bI_gain];
 add_gain = [1,1,1,1];
 
+paraminp = [mT_mcT_bT_bI]
+
+measures = []
 % a single variable should contain all parameters, and a single struct should contain all time varying inputs
 % as in, you should only be passing in two inputs to this function
 r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var,1, 0, [1, 1, 1], 15, 120, 13, 1, 1)
 
 function r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var, r_EQUAL, lo_constant, ang_vel,vel_gs, phi, dphi, TVS_I, TVS_P)
 
-    % please write code that is compact and easy to read
-    minT_safety = min(((inp+sys_bias).*sys_gain) + add_gain); % [inp(1)+sys_bias(1), inp(2)+sys_bias(2), inp(3)+sys_bias(3), inp(4)+sys_bias(4)];
-    % gained_inp = inp.*sys_gain;
-    % gained_bias = [gained_inp(1)+add_gain(1), gained_inp(2)+add_gain(2), gained_inp(3)+add_gain(3), gained_inp(4)+add_gain(4)];
-    % min_out = min(gained_bias);
+    minT_safety = min(((inp+sys_bias).*sys_gain) + add_gain);
 
-    min_saturation_out = min(max(minT_safety, var.rb(1)), var.rb(2)) % always end with a semicolon
+    min_saturation_out = min(max(minT_safety, var.rb(1)), var.rb(2)); 
 
     % this is wrong, these two lines should be identical to computing min_saturation_out, but using different variables for the max and min
     arrcheck = [r_EQUAL, lo_constant, min_saturation_out];
@@ -45,31 +43,20 @@ function r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var, r_EQUAL, 
     end
 
     % this code looks like a computer generated it, please make it compact like I did for computing the variable minT_safety 
-    yaw_lookup = var.yaw_table(vel_gs, 3); % wtf if with the constant 3
-
-    ref_yaw = yaw_lookup.*TVS_I;
-
-    ang_z = ang_vel(3); 
-
-    yaw_error = (-ang_z)+ref_yaw;
     
-    error_prod = yaw_error.*TVS_P;
-     
-    raw_dR = error_prod.*var.half_track;
+    % wtf if with the constant 3 
 
-    up = powerlimit.*var.r_power_sat;
+    raw_dR = (-(ang_vel(3)))+((var.yaw_table(vel_gs, 3)).*TVS_I).*TVS_P.*var.half_track;    
 
-    lo = up.*-1;  
+    lo = (powerlimit.*var.r_power_sat).*-1;  %variable name
 
     % again, this is wrong for the same reason as the above arrcheck
-    arrcheck = [up, raw_dR, lo];
+    arrcheck = [up, raw_dR, lo]; %variable removed to be edited
 
     % again, this looks wton
     e = max(arrcheck);
 
-    steering = abs(phi_sat) > dphi;
-
-    function_inp_u = e.*steering;
+    function_inp_u = e.*(abs(phi_sat) > dphi);
 
     if function_inp_u > 0
         r_TVS = [powerlimit powerlimit-abs(function_inp_u)];
