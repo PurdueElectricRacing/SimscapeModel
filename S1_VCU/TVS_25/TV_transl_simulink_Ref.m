2b7f5b037d0c95c6151d6518f446200245d1cb5b
PrakharDrolia <droliaprakhar15@outlook.com>
2025-01-21 19:42:24 -0500

    Updated functioning cleaned up version of previously computer generated nausea inducing code.
    Code WORKS!!!!!!!!11 No more nausea inducing.

--- S1_VCU/TVS_25/TV_transl_simulink_Ref.m
+++ S1_VCU/TVS_25/TV_transl_simulink_Ref.m
@@ -1,61 +1,49 @@
 % A direct translation of the simulink model:
 % Torque_Vectoring_2WD_NO_FILTER_NO_REGEN
 
-% please fill in varController_Master_TVS.m with parameters
-var = load("tvs_vars.mat");
+realt.mT_mcT_bT_bI = [0,0,0,0];
+realt.r_EQUAL = 0;
+realt.ang_vel = [1, 1, 1];
+realt.vel_gs = 15;
+realt.phi = 0;
+realt.dphi = 0;
+realt.TVS_I = 0;
+realt.TVS_P = 0;
+realt.T_max = 0;
+realt.r_TVS = [0, 0]
 
+realt = get_t_max(varController_Master_TVS,realt)
+r_TVS = get_TVS(varController_Master_TVS, realt, t_max)
 
+function realt = get_t_max(model, realt)
 
-inp = [50, 50, 50, 50];%mT_mcT_bT_bI =  %inp currently is initialised still even though final will be the mT... to still be able to test logic in program
+    realt.mT_mcT_bT_bI = clip(realt.mT_mcT_bT_bI, model.mT_mcT_bT_bI_maxlow, model.mT_mcT_bT_bI_maxupp);
 
-m_temp_max = [-50; 130];
-mc_temp_max = [-50; 130];
-bt_temp_max = [-50; 65];
-bI_current = [-1; 160];
+    minT_safety = min(((realt.mT_mcT_bT_bI+model.sys_bias).*model.sys_gain) + model.add_gain);
 
-inp(1) = clip(inp(1), m_temp_max(1), m_temp_max(2))
+    min_saturation_out = clip(minT_safety, model.rb(1), model.rb(2));
 
+    realt.T_max = clip(realt.r_EQUAL, 0, min_saturation_out);
+end
 
-% this block of code should be in varController_Master_TVS.m
-sys_bias = [-90, -60, -50, -130];
-sys_gain = [-0.1000, 0.1000, 0.1000, 0.1000];
-add_gain = [1,1,1,1];
+function realt = get_TVS(model, realt, powerlimit)
 
-%paraminp = [mT_mcT_bT_bI]
+    phi_sat = clip(realt.phi, -130, 130);
+ 
+    yaw_ref = interp2(model.distance,model.velocity,model.yaw_table,phi_sat,realt.vel_gs);
 
-measures = [] %should be a struct
-% a single variable should contain all parameters, and a single struct should contain all time varying inputs
-% as in, you should only be passing in two inputs to this function
-r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var,1, 0, [1, 1, 1], 15, 120, 13, 1, 1)
-
-function r_TVS = compute_r_TVS(inp, sys_bias, sys_gain, add_gain, var, r_EQUAL, lo_constant, ang_vel,vel_gs, phi, dphi, TVS_I, TVS_P)
-%function r_TVS = compute_r_TVS(inp, model, measures)
-    minT_safety = min(((inp+sys_bias).*sys_gain) + add_gain);
-
-    min_saturation_out = clip(minT_safety, var.rb(1), var.rb(2)); %brought in clip
-
-    powerlimit = clip(r_EQUAL, 0, min_saturation_out) %0 was lo_constant, should it be constant input or a variable
-
-    phi_sat = clip(phi, -130, 130); %brought in clip instead of if else
-
-    % this code looks like a computer generated it, please make it compact like I did for computing the variable minT_safety 
+    raw_dR = (-(realt.ang_vel(3)))+((yaw_ref).*realt.TVS_I).*realt.TVS_P.*model.ht(2);    
     
-    % wtf if with the constant 3 
+    lo = (powerlimit.*model.r_power_sat).*-1; 
+    up = (powerlimit.*model.r_power_sat);
+    e = clip(raw_dR, lo, up) 
 
-    raw_dR = (-(ang_vel(3)))+((var.yaw_table(vel_gs, 3)).*TVS_I).*TVS_P.*var.half_track;    
-
-    lo = (powerlimit.*var.r_power_sat).*-1;  %variable name
-    up = (powerlimit.*var.r_power_sat);
-
-    %arrcheck = [up, raw_dR, lo]; %variable removed to be edited - up
-    e = clip(raw_dR, lo, up) %brought in clip
-
-    function_inp_u = e.*(abs(phi_sat) > dphi);
+    function_inp_u = e.*(abs(phi_sat) > realt.dphi);
 
     if function_inp_u > 0
-        r_TVS = [powerlimit powerlimit-abs(function_inp_u)];
+        realt.r_TVS = [powerlimit(1) powerlimit(1)-abs(function_inp_u)];
     else
-        r_TVS = [powerlimit-abs(function_inp_u) powerlimit];
+        realt.r_TVS = [powerlimit(1)-abs(function_inp_u) powerlimit(1)];
     end
 end
                 
