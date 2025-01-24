51538cd9a3a5e526d9aac937b7b07e050492a888
WuWilliam0713 <wuwilliam.1@gmail.com>
2025-01-23 21:30:42 -0500

    update maxT function

--- S1_VCU/TVS_25/get_PT.m
+++ S1_VCU/TVS_25/get_PT.m
@@ -1,5 +1,8 @@
 function y = get_PT(p,y)
-
-
-
+    %% Load interpolant data
+    load(p);  % VMU/lib_p/Motor_tables/TorqueTable.mat
+    %% Get Maximum Torque
+    speed_query = y.w_sf;
+    voltage_query = y.VB_sf;
+    maxT = torqInterpolant(speed_query, voltage_query);
 end
