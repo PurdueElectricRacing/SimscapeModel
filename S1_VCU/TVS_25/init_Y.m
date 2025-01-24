2998a0c1209c361444df8b0bc0d54f36f559df1c
arclnx <73797060+arclnx@users.noreply.github.com>
2025-01-23 19:33:55 -0500

    changed y_init to classdef
    changed y initialization function to yVCU_Master, now is a class

--- S1_VCU/TVS_25/init_Y.m
+++
@@ -1,6 +0,0 @@
-function y = init_Y()
-
-    % Traction Control (TC) variables
-    y.TC_highs = 0; % counter to track number of consecutive high sl values
-    y.TC_lows = 0; % counter to track number of consecutive low sl values
-end
---
+++ S1_VCU/TVS_25/yVCU_Master.m
@@ -0,0 +1,17 @@
+classdef yVCU_Master < handle
+
+    %% y properties
+    properties
+    % Traction Control (TC) variables
+        TC_highs; % counter to track number of consecutive high sl values
+        TC_lows; % counter to track number of consecutive low sl values
+    end
+
+    %% y Methods
+    methods
+        function y = yVCU_Master()
+            y.TC_highs = 0;
+            y.TC_lows = 0;
+        end
+    end
+end
