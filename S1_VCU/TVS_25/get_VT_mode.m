b581185df31e5e88f2fd0c566e122d9926c75f66
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2025-01-25 13:00:31 -0500

    add VT mode selection and y signal description

--- S1_VCU/TVS_25/get_VT_mode.m
+++ S1_VCU/TVS_25/get_VT_mode.m
@@ -1,4 +1,3 @@
-%
 % Youngshin Choi
 %
 % function description
@@ -8,14 +7,11 @@
 % currentmode: the current mode the vehicle is at
 % steering: the steering angle of the vehicle
 % meansteer: the mean steering angle of the vehicle
-%
 
 function y = get_VT_mode(y)
-    if abs(steering) < meansteer + deltasteer
-        updatedmode = 2;  % car is turning
-    elseif abs(steering) > meansteer + deltasteer
-        updatedmode = 1;   % car is going in a straight line
-    else
-        updatedmode = currentmode; % neither, the car is still in the previous mode
+    if abs(y.ST_CF) < y.DB_CF + p.dST_DB
+        y.VT_mode = 2;  % car is turning
+    elseif abs(y.ST_CF) > y.DB_CF + p.dST_DB
+        y.VT_mode = 1;  % car is going in a straight line
     end
 end
--- S1_VCU/TVS_25/yVCU_Master.m
+++ S1_VCU/TVS_25/yVCU_Master.m
@@ -4,6 +4,37 @@ classdef yVCU_Master < handle
     % Clip and filter (CF) variables
         IB_CF_vec; % vector of the past N battery current measurements
 
+        TH_CF; % Throttle sensor Unit: [unitless] Size: [1 1]
+                % Max Torque = 1, No Torque = 0
+        ST_CF; % Steering angle sensor Unit: [degree] Size: [1 1]
+                % Right turn  = positive value, Left turn = negative value
+        VB_CF; % Battery voltage Unit: [V] Size: [1 1]
+                % Always positive number
+        WT_CF; % Tire angular velocity Unit: [rad/s] Size: [1 2] Order: [Left Right]
+                % Moving forward = positive value, Not moving = 0
+        GS_CF; % Vehicle ground speed Unit: [m/s] Size: [1 1]
+                % Moving forward = positive value, Not moving = 0
+        AV_CF; % Chassis angular velocity Unit: [rad/s] Size: [1 3] Order: [x y z]
+                % Right turn = positive value, Left turn = Negative value
+        IB_CF; % Battery current Unit: [A] Size: [1 1]
+                % Positive torque = Positive current, No torque = 0
+        MT_CF; % Max motor temperature Unit: [C] Size: [1 1]
+                % Temperature for each motor is mesured, only max is recieved
+        CT_CF; % Max motor controller temperature Unit: [C] Size: [1 1]
+                % Temperature for each motor controller is mesured, only max is recieved
+        BT_CF; % Max battery cell temperature Unit: [C] Size: [1 1]
+                % Temperature for each battery cell is mesured, only max is recieved
+        AG_CF; % Chassis acceleration Unit: [m/s^2] Size: [1 3] Order: [x y z]
+                % Increasing velocity = positive value, No acceleration = 0
+        TO_CF; % Motor torque Unit: [Nm] Size: [1 2] Order: [Left Right]
+                % Torque to move forward = positive value, No torque = 0
+        DB_CF; % Torque vectoring steering angle deadband Unit: [degree] Size: [1 1]
+                % Deadband = positive number, No deadband = 0
+        PI_CF; % Torque vectoring intensity Unit: [unitless] Size: [1 1]
+                % Normal Behaviour = 1, Always go straight = 0
+        PP_CF; % Torque vectoring proportional gain Unit: [unitless] Size: [1 1]
+                % Normal behaviour = 0.4, Always go straight = 0
+
     % Traction Control (TC) variables
         TC_highs; % counter to track number of consecutive high sl values
         TC_lows; % counter to track number of consecutive low sl values
@@ -14,6 +45,22 @@ classdef yVCU_Master < handle
         function y = yVCU_Master(p)
             % Clip and filter (CF) variables
             y.IB_CF_vec = ones(1,p.SF_IB_filter);
+
+            y.TH_CF = 0;
+            y.ST_CF = 0;
+            y.VB_CF = 600;
+            y.WT_CF = [0 0];
+            y.GS_CF = 0;
+            y.AV_CF = [0 0 0];
+            y.IB_CF = 0;
+            y.MT_CF = 20;
+            y.CT_CF = 20;
+            y.BT_CF = 20;
+            y.AG_CF = [0 0 9.81];
+            y.TO_CF = [0 0];
+            y.DB_CF = 12;
+            y.PI_CF = 1;
+            y.PP_CF = 1;
 
             % Traction Control (TC) variables
             y.TC_highs = 0;
