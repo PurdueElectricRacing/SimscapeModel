a0e26149b99a84bd7d76aa35f2720519e2588127
WuWilliam0713 <wuwilliam.1@gmail.com>
2025-01-25 12:21:50 -0500

    simpify the code

--- S1_VCU/TVS_25/Construct_pVCU/Process Functions/compute_AMK_func.m
+++ S1_VCU/TVS_25/Construct_pVCU/Process Functions/compute_AMK_func.m
@@ -1,6 +1,6 @@
-function [speed_bp,voltage_bp,torque_bp,minT_tbl,maxT_tbl, speedT_tbl, voltageT_tbl] = compute_AMK_func(data_path)
+function [torqInterpolant] = compute_AMK_func(data_path)
 %% Load 
-load (data_path, "Speed", "Shaft_Torque", "Voltage_Phase_RMS", "Total_Loss");
+load (data_path, "Speed", "Shaft_Torque", "Voltage_Phase_RMS");
 %% Define Matrices
 % get raw data
 [m,n] = size(Speed);
@@ -8,12 +8,6 @@ speed_matrix = Speed.*(pi/30); % speed breakpoints (rad/s)
 current_matrix = repmat(0:5.25:105,[m,1]); % current matrix (A)
 torque_matrix = Shaft_Torque; % Output motor torque (Nm)
 voltageDC_matrix = Voltage_Phase_RMS.*sqrt(2); % DC voltage (V)
-loss_matrix = Total_Loss; % power wasted (W)
-
-minT_matrix = -torque_matrix(:,2:end) + 2*torque_matrix(:,1);
-torque_matrix_all = [torque_matrix  minT_matrix];
-speed_matrix_all = [speed_matrix speed_matrix(:,2:end)];
-power_matrix_all = [loss_matrix loss_matrix(:,2:end)];
 
 % speed breakpoint definition
 min_speed = 0;
@@ -27,24 +21,14 @@ max_voltage = 598;
 nv = 50;
 voltage_bp = linspace(min_voltage,max_voltage,nv)';
 
-% torque breakpoint definition
-min_torque = -23;
-max_torque = 23;
-nt = 101;
-torque_bp = linspace(min_torque,max_torque,nt)';
-
 % Table 1: speed-voltage-torque
 [speedT_tbl, voltageT_tbl] = meshgrid(speed_bp, voltage_bp);
-
-% Table 2: speed-torque-current
-[speedI_tbl, torqueI_tbl] = meshgrid(speed_bp, torque_bp);
 
 %% Step 0: remove 0 speed torque because it is obviously wrong
 speed_matrix = speed_matrix(2:end,:);
 current_matrix = current_matrix(2:end,:);
 torque_matrix = torque_matrix(2:end,:);
 voltageDC_matrix = voltageDC_matrix(2:end,:);
-loss_matrix = loss_matrix(2:end,:);
 
 %% Step 1: make speed-currrent-max_voltage_dc a bijective function
 VoltageDC_matrix_rounded = round(voltageDC_matrix,2);
@@ -79,9 +63,5 @@ for i = 1:nv
     maxT_tbl(i,:) = max(0, feval(torque_fit,speed_bp,barrier_current));
 end
 
-%% Step 5: flip and translate limit table to get regen as well
-minT_tbl = -maxT_tbl + 2*min(maxT_tbl,[],"all");
-
-%% 
-
+torqInterpolant = griddedInterpolant(speedT_tbl', voltageT_tbl', maxT_tbl', 'linear');
 end
