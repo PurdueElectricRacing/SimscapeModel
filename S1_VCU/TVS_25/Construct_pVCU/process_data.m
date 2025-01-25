a9374dae16b3ac1a043d3f7b6f4f95d545f430e3
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2025-01-25 09:51:33 -0500

    constructing raw source data and how directory where that data is processed

---
+++ S1_VCU/TVS_25/Construct_pVCU/process_data.m
@@ -0,0 +1,16 @@
+%% Compute IMU calibration
+% call function that does this, located in Process Functions
+
+% save data in Processed Data
+
+%% Compute AMK max torque lookup table
+% call function that does this, located in Process Functions
+
+% save data in Processed Data
+
+%% Compute Yaw Table
+% empty until steady state 6DOF model is ready
+
+% call function that does this, located in Process Functions
+
+% save data in Processed Data
