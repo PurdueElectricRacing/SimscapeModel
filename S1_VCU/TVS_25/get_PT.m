d67284b2a0b26eb5cdcb7c7d04d1f09109955a04
arclnx <73797060+arclnx@users.noreply.github.com>
2025-01-25 12:29:18 -0500

    Update get_PT.m

--- S1_VCU/TVS_25/get_PT.m
+++ S1_VCU/TVS_25/get_PT.m
@@ -6,20 +6,11 @@ function y = get_PT(p,y)
     v_bat = y.VB_sf; % battery voltage
     min_motor_max_T = min(p.torque_interpolant(w_motor, [v_bat v_bat]); % smallest of motor max toqrue limited by voltage and velocity
 
-    % derate for motor t, motor contol t, bat t, bat I
-    clip(interp1([p.mT_derating_full_T, p.mT_derating_zero_T], [1, 0], y.motor_T), 0, 1);
-    clip(interp1([p.mcT_derating_full_T, p.mcT_derating_zero_T], [1, 0], y.motor_controller_T), 0, 1);
-    clip(interp1([p.bT_derating_full_T, p.bT_derating_zero_T], [1, 0], y.batt_T), 0, 1);
-    clip(interp1([p.bI_derating_full_T, p.bI_derating_zero_T], [1, 0], y.batt_I), 0, 1);
-
-mT_derating_full_T; 
-mT_derating_zero_T; 
-mcT_derating_full_T;
-mcT_derating_zero_T;
-bT_derating_full_T; 
-bT_derating_zero_T; 
-bI_derating_full_T; 
-bI_derating_zero_T; 
+    % derate for motor temp, motor contoller temo, batt t, bat I
+    mT_derated = p.MAX_TORQUE_NOM * clip(interp1([p.mT_derating_full_T, p.mT_derating_zero_T], [1, 0], y.motor_T), 0, 1);
+    mcT_derated = p.MAX_TORQUE_NOM * clip(interp1([p.mcT_derating_full_T, p.mcT_derating_zero_T], [1, 0], y.motor_controller_T), 0, 1);
+    bt_derated = p.MAX_TORQUE_NOM * clip(interp1([p.bT_derating_full_T, p.bT_derating_zero_T], [1, 0], y.batt_T), 0, 1);
+    bI_derated = p.MAX_TORQUE_NOM * clip(interp1([p.bI_derating_full_T, p.bI_derating_zero_T], [1, 0], y.batt_I), 0, 1);
 
     y.T_max = clip(y.r_EQUAL, 0, min_saturation_out);
 end
