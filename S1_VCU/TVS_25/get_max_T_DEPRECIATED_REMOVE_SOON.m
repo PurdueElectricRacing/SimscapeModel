588a72ec32a29264856d03f12d3d4b2658cb08f8
arclnx <73797060+arclnx@users.noreply.github.com>
2025-01-25 13:01:57 -0500

    Delete get_max_T_DEPRECIATED_REMOVE_SOON.m

--- S1_VCU/TVS_25/get_max_T_DEPRECIATED_REMOVE_SOON.m
+++
@@ -1,6 +0,0 @@
-function y = get_max_T(p,y)
-    y.mT_mcT_bt_bI = clip(y.mT_mcT_bt_bI, p.mT_mcT_bT_bI_maxlow, mT_mcT_bT_bI_maxupp);
-    minT_safety = min(((y.mT_mcT_bT_bI + p.sys_bias) .* p.sys_gain) + p.add_gain);
-    min_saturation_out = clip(minT_safety, p.rb(1), p.rb(2));
-    y.T_max = clip(y.r_EQUAL, 0, min_saturation_out);
-end
