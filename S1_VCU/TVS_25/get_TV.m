6a2c3a98959ad164aa6fc0d103eb336838652a50
arclnx <73797060+arclnx@users.noreply.github.com>
2025-02-01 11:06:23 -0500

    Update get_TV.m

--- S1_VCU/TVS_25/get_TV.m
+++ S1_VCU/TVS_25/get_TV.m
@@ -1,10 +1,11 @@
-function y = get_TV(p,y)
-    phi_sat = clip(y.phi, -130, 130);
-    yaw_ref = interp2(p.TV_phi_brkpt, p.TV_vel_brkpt, p.TV_yaw_table, phi_sat, y.vel_gs);
-    raw_dR = (-y.ang_vel(3) + yaw_ref.*y.TVS_I) .* y.TVS_P .* p.ht(2);
+function y = get_TV(p, y)
 
-    lower = -powerlimit .* p.r_power_sat;
-    upper = powerlimit .* p.r_power_sat;
+    phi_sat = clip(y.ST_CF, -130, 130);
+    yaw_ref = interp2(p.TV_phi_brkpt, p.TV_vel_brkpt, p.TV_yaw_table, phi_sat, y.GS_CF);
+    raw_dR = (yaw_ref * y.PI_CF - y.AV_CF(3)) * y.PP_CF * p.ht(2);
+
+    lower = -y.powerlimit .* p.r_power_sat;
+    upper = y.powerlimit .* p.r_power_sat;
     e = clip(raw_dR, lower, upper);
 
     function_inp_u = e * (abs(phi_sat) > y.dphi);
