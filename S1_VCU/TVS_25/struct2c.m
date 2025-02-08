18d47ec9505fc3e3acd1e537111ce01f751f3e46
arclnx <73797060+arclnx@users.noreply.github.com>
2025-02-08 14:15:28 -0500

    added struct2c
    struct2c takes a struct as input and outputs a .c file containing the values of the struct, word wrapped to 80 characters

---
+++ S1_VCU/TVS_25/struct2c.m
@@ -0,0 +1,60 @@
+function [] = struct2c(struct, name, output)
+% Takes a struct, generates c code to fill values into struct
+
+fid = fopen(output, 'w');
+
+fields = string(fieldnames(struct));
+numFields = length(fields);
+for i = 1:numFields
+    % print single values with 7 decimals, trailing zeros and . removed
+    if isequal(size(struct.(fields(i))), [1, 1])
+        val = struct.(fields(i));
+        valstr = rmzeros(sprintf("%.7f", val));
+        fprintf(fid, "%s->%s = %s;\n", name, fields(i), valstr);
+    
+    % print array with 7 decimals, word wrap at 80 characters
+    else
+        wrapped = false;
+        val = struct.(fields(i));
+        len_val = length(val);
+
+        header = sprintf("%s->%s = [", name, fields(i));
+        fprintf(fid, header);
+
+        len_line = strlength(header);
+        for j = 1:len_val
+            el = val(j);
+            elstr = rmzeros(sprintf("%.7f", el));
+
+            % length of current line + unprinted new element "elstr"
+            len_line = len_line + strlength(elstr) + 1 + (j~=len_val);
+            
+            if len_line > 80 % wrap to newline
+                wrapped = true;
+                if j ~= 1
+                    fprintf(fid, ",");
+                end
+                fprintf(fid, "\n");
+                len_line = strlength(elstr) + 2;
+                fprintf(fid, "%s", elstr);
+            else
+                if j ~= 1
+                    fprintf(fid, ", ");
+                end
+                fprintf(fid, "%s", elstr);
+            end
+        end
+        fprintf(fid, "];\n");
+
+        if wrapped % print extra line of space if array was wrapped
+            fprintf(fid, "\n"); 
+        end
+    end
+end
+
+fclose(fid);
+
+function [str] = rmzeros(str) 
+    str = strip(strip(str, "right", "0"), "right", ".");
+end
+end
