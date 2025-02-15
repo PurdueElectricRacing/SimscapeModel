57bca2a0512fc1b23459218e97c14aee7602e351
OoglyGoogly <choi977@purdue.edu>
2025-02-15 11:09:17 -0500

    Update get_VS.m

--- S1_VCU/TVS_25/get_VS.m
+++ S1_VCU/TVS_25/get_VS.m
@@ -2,10 +2,7 @@ function y = get_VS(p,y)
     % compute reference wheel speed
     y.WM_VS = [0, 0]
     
-    p.REF_shaftspeed;
-    p.REF_slip_ratio;
-    
-    if (y.GS_CF < (set threshold where it is considered low speed accleration))
+    if (y.GS_CF < p.REF_low_shaftspeed)
         y.WM_VS = [p.REF_shaftspeed, p.REF_shaftspeed];
     else
         y.WM_VS = [(p.REF_slip_ratio .* y.GS_CF + y.GS_CF) / p.r, ...
