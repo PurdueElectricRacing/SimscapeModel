32bb79c0e9175004aba870b06b262b74b19b4a81
DemetriusGulewicz <133685039+DemetriusGulewicz@users.noreply.github.com>
2025-02-20 22:05:30 -0500

    fixing res

--- S3_VMU/Master_Model_3DOF/main_steady_state.m
+++ S3_VMU/Master_Model_3DOF/main_steady_state.m
@@ -1,15 +1,17 @@
+%% Initialize model and such
 global S
 S = [0;0];
-
+opts = optimoptions("fsolve",'Algorithm', 'trust-region','FiniteDifferenceType','central','FiniteDifferenceStepSize',1e-8);
 varCAR = varModel_master_3DOF;
-s = [0.001; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; 0; 0; 0];
-ref_val = [0;0;0;0;0;0;0;0;0;0;0;0];
+s = [5; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; 0; 0; 0];
+
+% if tauRaw = [0;0] and dx = 0, this works
 tauRaw = [0;0];
+vals = fsolve(@compute_ds_master_3DOF,s,opts,tauRaw,varCAR);
+res  = compute_ds_master_3DOF(vals, tauRaw, varCAR);
 
-tauf = linspace(0,11,11);
-
-% vals = fsolve(@computer_res_master,s,optimoptions("fsolve"),tauRaw, varCAR,ref_val);
-% [resv]  = computer_res_master(vals, tauRaw, varCAR, ref_val);
-
-vals = fsolve(@compute_ds_master_3DOF,s,optimoptions("fsolve"),tauRaw,varCAR);
-[resv]  = compute_ds_master_3DOF(vals, tauRaw, varCAR);
+% 
+tauRaw = [0;15];
+ref_val = [0;0;0;0;0;0;0;0;0;0;0;0];
+vals = fsolve(@compute_res_master,s,opts,tauRaw,varCAR,ref_val);
+res  = compute_res_master(vals, tauRaw, varCAR, ref_val);
