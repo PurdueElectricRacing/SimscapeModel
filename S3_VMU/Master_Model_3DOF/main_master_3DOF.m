35225ed52ce0a6297b5fd004dd368317ac65d5bf
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2025-02-27 19:54:25 -0500

    3DOF cooking

--- S3_VMU/Master_Model_3DOF/main_master_3DOF.m
+++ S3_VMU/Master_Model_3DOF/main_master_3DOF.m
@@ -15,14 +15,14 @@ varCAR = varModel_master_3DOF;
 s0 = [0.001; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; varCAR.v0; 0; 0];
 
 %% Boundary Conditions
-tau = [5; 5];
+tau = [0; 25];
 
 %% Configure Solver
-optionsODE = odeset('MaxStep', 0.0005, 'AbsTol', 100, 'RelTol', 100);
+optionsODE = odeset('MaxStep', 1, 'AbsTol', 1e-6, 'RelTol', 1e-6);
 
 %% Simulate
 t0 = tic;
-[t,s] = ode23tb(@compute_ds_master_3DOF, [0 20], s0, optionsODE, tau, varCAR);
+[t,s] = ode23tb(@compute_ds_master_3DOF, [0 100], s0, optionsODE, tau, varCAR);
 tau = (tau.*ones(2,length(t)))';
 t1 = toc(t0);
 
