160935f825a5a4083ad44523c4e26ecc98324482
DEMETRIUS GULEWICZ <70825608+invertedparadox@users.noreply.github.com>
2025-03-01 13:47:29 -0500

    fsolve working

--- S3_VMU/Master_Model_3DOF/main_steady_state.m
+++ S3_VMU/Master_Model_3DOF/main_steady_state.m
@@ -1,7 +1,7 @@
 %% Initialize model and such
 global S
 S = [0;0];
-opts = optimoptions("fsolve",'Algorithm', 'trust-region-dogleg','FiniteDifferenceType','central','FiniteDifferenceStepSize',1e-4);
+opts = optimoptions("fsolve",'Algorithm', 'trust-region','FiniteDifferenceType','central','FiniteDifferenceStepSize',1e-4);
 varCAR = varModel_master_3DOF;
 
 % if velocity is 0, torque = [0;0] this works
@@ -11,7 +11,7 @@ vals = fsolve(@compute_ds_master_3DOF,s,opts,tauRaw,varCAR);
 res  = compute_ds_master_3DOF(vals, tauRaw, varCAR);
 
 % if velocity is 0, torque = [0;15] this works
-s = [0; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; 0; 0; 0; 12.5;12.5];
+s = [10; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; 0; 0; 0; 12.5;12.5];
 ref_val = [0;10;0;0;0;0;0;0;0;0;0;0];
 vals = fsolve(@compute_res_master,s,opts,varCAR,ref_val);
 res  = compute_res_master(vals, varCAR, ref_val);
