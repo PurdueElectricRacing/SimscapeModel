7ca023a888c081d5bb66ba313dd36d7c3fa579fa
OoglyGoogly <choi977@purdue.edu>
2025-03-01 15:29:55 -0500

    updates in variable nemaes in get_VS, added column to Y_TVS_5_10_25_N3

--- S1_VCU/TVS_25/get_VS.m
+++ S1_VCU/TVS_25/get_VS.m
@@ -3,7 +3,7 @@ function y = get_VS(p,y)
     y.WM_VS = [0, 0];
     
     if (y.GS_CF < p.REF_low_shaftspeed)
-        y.WM_VS = [p.REF_shaftspeed, p.REF_shaftspeed];
+        y.WM_VS = [p.REF_shaft_speed, p.REF_shaft_speed];
     else
         y.WM_VS = [(p.REF_slip_ratio .* y.GS_CF + y.GS_CF) / p.r, ...
                     ((p.REF_slip_ratio .* y.GS_CF + y.GS_CF)) / p.r];
