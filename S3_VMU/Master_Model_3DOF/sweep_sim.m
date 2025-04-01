8296ad96f6b4a3c42edecdab7ba4e643741968be
PrakharDrolia <droliaprakhar15@outlook.com>
2025-04-01 19:06:17 -0400

    PLOTTTTSSSSS!!!!
    Succesfully simulates and plots!

--- S3_VMU/Master_Model_3DOF/sweep_sim.m
+++ S3_VMU/Master_Model_3DOF/sweep_sim.m
@@ -5,8 +5,8 @@ addpath(genpath(pwd))
 varCAR = varModel_master_3DOF;
 
 %% Initial Conditions
-vvec = linspace(1,1000,5);
-rvec = linspace(1,25,5);
+vvec = linspace(0,16000,20);
+rvec = linspace(1,25,10);
 
 [V_grid, r_grid] = meshgrid(vvec,rvec);
 
@@ -19,30 +19,45 @@ n = length(v_all);
 
 vals_all = zeros(n,numel(s0));
 %% Boundary Conditions
+% for idx = 1:25
+% 
+%     tau(idx,:) = [idx;25-idx]
+% 
+% end
+
+Assa = linspace(2000,2500,10);
+tau = linspace(25,0,10)  
+count = 1
 
 
 %% Configure Solver
 optionsODE = odeset('MaxStep', 5, 'AbsTol', 1e-4, 'RelTol', 1e-4);
-
 %% Simulate
 for i = 1:n
     s0 = [0.001; 0; 0; varCAR.zs; 0; varCAR.O0; 0; 0; varCAR.v0; v_all(i); 0; 0];
     % Solve the ODE
-    [t, ss] = ode23tb(@compute_ds_mastersim_3DOF, [0 10], s0, optionsODE, tau, varCAR);
+    taut = [0;r_all(i)];
+    [t, ss] = ode23tb(@compute_ds_mastersim_3DOF, [0 10], s0, optionsODE, taut, varCAR);
+    %ddx_call = compute_ds_mastersim_3DOF(t, s0, taut, varCAR); 
+    %ddx_plot = ddx_call(1);
     endsize = size(ss);
     % Store the final values of the solution vector s and the final value of t
-    vals_all(i,:) = ss(endsize(1),:); % Storing the last column of s
-    
+    vals_all = ss(endsize(1),:); % Storing the last column of ss
+    %disp(ddx_plot);
+    power_plot(i) = vals_all (9) * vals_all(12);
+    %count = count + 1;
 end
-t0 = tic;
+%t0 = tic;
 
-tau = (tau.*ones(2,length(t)))';
-t1 = toc(t0);
+%tau = (tau.*ones(2,length(t)))';
+%t1 = toc(t0);
 
 %% Pack output
 %%v_master = compute_v_master_3DOF(t, s, tau, varCAR);
 
 %% plot data
-IMf_Imr = vals_all(:,10)
-pow_pow = vals_all(:,9).*(IMf_Imr(1)+IMf_Imr(2));
-scatter3(v_all,t11,pow_pow);
+%IMf_Imr = vals_all(:,10)
+%pow_pow = vals_all(:,9).*(IMf_Imr(1)+IMf_Imr(2));
+scatter3(r_all,v_all,power_plot);
+%plot(tau,power_plot)
+
