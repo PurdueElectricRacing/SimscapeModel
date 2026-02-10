4e208cc21c95da2cbf54c5c4f1d82b0c7cfb92aa
arclnx <73797060+arclnx@users.noreply.github.com>
2026-02-10 17:34:39 -0500

    fixed coordinate conversion

--- S3_VMU/Suspension Model/convertReference.m
+++ S3_VMU/Suspension Model/convertReference.m
@@ -1,24 +1,27 @@
 vehicleCOG = [5 9 1];
 
-pitch = pi/4;
-roll = 0;
-yaw = 0;
+n = [0 0 1];
 
-z = yaw;
-y = pitch;
-x = roll;
 
-cx = cos(x);
-sx = sin(x);
-cy = cos(y);
-sy = sin(y);
-cz = cos(z);
-sz = sin(z);
+conv = @convertCoords;
 
-R = [cz*cy, cz*sy*sx-sz*cx, cz*sy*cx+sz*sx; ...
-     sz*cy, sz*sy*sx+cz*cx, sz*sy*cx-cz*sx; ...
-     -sy,   cy*sx,          cy*cx];
+function [new_pt] = convertCoords(pt, pitch, roll, yaw)
+    zrot = yaw;
+    yrot = pitch;
+    xrot = -roll;
+    
+    cx = cos(xrot);
+    sx = sin(xrot);
+    cy = cos(yrot);
+    sy = sin(yrot);
+    cz = cos(zrot);
+    sz = sin(zrot);
+    
+    R = [cz*cy, cz*sy*sx-sz*cx, cz*sy*cx+sz*sx; ...
+         sz*cy, sz*sy*sx+cz*cx, sz*sy*cx-cz*sx; ...
+         -sy,   cy*sx,          cy*cx]';
 
-R'*[0 0 -1]'
-
-function [new_pt] = convertCoords
+    % pt = pt .* [1 1 -1]; % flip z coordinates
+    new_pt = R * pt'; % transform
+    % new_pt = new_pt .* [1 1 -1]'; % re-flip z
+end
