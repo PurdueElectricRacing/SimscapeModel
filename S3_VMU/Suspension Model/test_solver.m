a15b1a9819593a0481457bc47e6a545a1dff1650
arclnx <73797060+arclnx@users.noreply.github.com>
2026-02-12 17:09:24 -0500

    pitch, roll solving

--- S3_VMU/Suspension Model/test_solver.m
+++ S3_VMU/Suspension Model/test_solver.m
@@ -17,13 +17,19 @@ l(9) = 4.5;
 l(10) = 2.2;
 l(11) = 6.0828;
 
-close all
+alpha = 0;
 
-for ey = -4
-fixed_pts(5,2) = ey;
-for alpha = linspace(-pi/8, pi/8, 9)
+pitch = 0;
+roll = 0;
+h = 1;
 
-solved_pts = solveSusPoints(fixed_pts, l, alpha);
+n1 = cross([cos(roll), 0, sin(roll)], [0, cos(pitch), sin(pitch)]);
+p1 = -n1(3) * h;
+[cos(roll), 0, sin(roll)];
+
+alpha_sol = fzero(@(alpha)(residual(fixed_pts, l, alpha, n1, p1)), [-pi/4, pi/4]);
+
+solved_pts = solveSusPoints(fixed_pts, l, alpha_sol);
 
 scatter3(fixed_pts(:,1), fixed_pts(:,2), fixed_pts(:,3), 40, "red", "filled")
 hold on;
@@ -31,19 +37,16 @@ RGB = orderedcolors("glow");
 scatter3(solved_pts(:, 1), solved_pts(:, 2), solved_pts(:, 3), 40, RGB(1:4,:), "filled")
 rod_segments = [1 5; 1 6; 4 7; 4 8; 1 4; 1 3; 1 2; 4 3; 4 2; 3 9; 3 2];
 rods = plotSegments([solved_pts; fixed_pts],rod_segments);
-if alpha == 0
 plot3(rods(:,1), rods(:,2), rods(:,3), Color="#edb120", LineWidth=1)
-end
-end
-end
-
 set(gcf, "Theme", "Light")
-
-
-% hold off;
 xlabel("x")
 ylabel("y")
 zlabel("z")
 xlim([0,6])
 ylim([-4,2])
-% zlim([-4,6])
+
+function error = residual(f, l, alpha, n1, p1)
+    pts = solveSusPoints(f, l, alpha);
+    error = dot(pts(2,:), n1) - p1;
+    
+end
