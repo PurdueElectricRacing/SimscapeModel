692ad0bde679d025eda3b5c70252c32808ac6c7b
arclnx <73797060+arclnx@users.noreply.github.com>
2026-04-01 13:58:54 -0400

    controller derating

---
+++ S1_VCU/TVS_26/Controllers/get_BL_PO.m
@@ -0,0 +1,54 @@
+%% Function Description
+% vcu_step runs every loop on the TV board
+%
+% Inputs
+%   p   vehicle paramater struct. constant
+%   x   Raw sensor data struct. filled with data read from CAN
+%           in main.c
+%   y   Function input and output struct. contains all clipped and
+%           filtered variables, variable buffers, and output from
+%           this function.
+% Outputs
+%   y   modified version of input y
+
+function y = get_BL_PO(p, x, y)
+    % max torque allowed by throttle position
+    TO_ET_PO = y.TH_PO * p.MAX_TO .* [1 1 1 1];
+
+    % 80kW rules limit derating
+    % only derating to 50% total torque, F:R derating split can be changed
+    PB_snipped = snip(y.PB, p.PB_derating_full_P, p.PB_derating_half_P);
+    PB_derate_front = interp1([p.PB_derating_full_P, p.PB_derating_half_P], [1,1-PB_derating_FR], PB_snipped);
+    PB_derate_rear = interp1([p.PB_derating_full_P, p.PB_derating_half_P], [1,PB_derating_FR], PB_snipped);
+    PB_derate = [PB_derate_front, PB_derate_front, PB_derate_rear, PB_derate_rear];
+
+    % Inverter temp safetey derating - derate all motors based on highest inverter temp
+    INV_T_snipped = snip(y.INV_T, p.INV_T_derating_full_T, p.INV_derating_zero_T);
+    INV_T_derate = [1 1 1 1] * interp1([p.INV_T_derating_full_T, p.INV_derating_zero_T], [1,0], INV_T_snipped);
+
+    % IGBT temp safety derating - derate all motors based on highest IGBT temp
+    IGBT_T_snipped = snip(y.IGBT_T, p.IGBT_derating_full_T, p.IGBT_derating_zero_T);
+    IGBT_T_derate = [1 1 1 1] * interp1([p.IGBT_derating_full_T, p.IGBT_derating_zero_T], [1,0], IGBT_T_snipped);
+
+    % Motor temp safetey derating - derate all motors based on highest motor temp
+    MT_snipped = snip(y.MT, p.MT_derating_full_T, p.MT_derating_zero_T);
+    MT_derate = [1 1 1 1] * interp1([p.MT_derating_full_T, p.MT_derating_zero_T], [1,0], MT_snipped);
+
+    % Battery temp safety derating - derate all motors based on highest cell temp
+    BT_snipped = snip(y.BT, p.BT_derating_full_T, p.BT_derating_zero_T);
+    BT_derate = [1 1 1 1] * interp1([p.BT_derating_full_T, p.BT_derating_zero_T], [1,0], BT_snipped);
+
+    % Battery undervoltage safety derating
+    VB_snipped = snip(y.VB, p.VB_derating_full_T, p.VB_derating_zero_T);
+    VB_derate = [1 1 1 1] * interp1([p.VB_derating_full_T, p.VB_derating_zero_T], [1,0], VB_snipped);
+
+    % Battery current safety derating
+    IB_snipped = snip(y.IB, p.IB_derating_full_T, p.IB_derating_zero_T);
+    IB_derate = [1 1 1 1] * interp1([p.IB_derating_full_T, p.IB_derating_zero_T], [1,0], IB_snipped);
+    
+    % combine derating, multiply by abs max torque to get max torque
+    y.TO_DR_MAX = p.MAX_TORQIE_ABS * min(PB_derate, INV_T_derate, IGBT_T_derate, MT_derate, BT_derate, VB_derate, IB_derate);
+
+    % compute overall maximum torque
+    y.TO_BL = 
+end
--- S1_VCU/TVS_26/Controllers/vcu_step.m
+++ S1_VCU/TVS_26/Controllers/vcu_step.m
@@ -1,31 +1,31 @@
+%% Function Description
 % vcu_step runs every loop on the TV board
+%
 % Inputs
 %   p   vehicle paramater struct. constant
-%   f   CAN flag variable struct. filled with data read from CAN
-%           in main.c
 %   x   Raw sensor data struct. filled with data read from CAN
 %           in main.c
 %   y   Function input and output struct. contains all clipped and
 %           filtered variables, variable buffers, and output from
 %           this function.
+% Outputs
+%   y   modified version of input y
 
-function y = vcu_step(p,f,x,y)
-    y = get_CF(p, f, x, y);
+function y = vcu_step(p, x, y)
+    % clip and filter results
+    y = get_CF(p, x, y);
 
     % determine VCU mode
     y = get_VCU_mode(p,f,x,y);
     
-    % calculate baseline control
-    y = get_BL(p,y);
-
-    % calculate proportional control
-    y = get_PT(p,y);
-
-    % calculate variable speed (TC)
-    y = get_VS(p, y);
-
-    % calculate variable torque (TV)
-    y = get_VT(p, y);
-
-    % calculate regen
+    % switch between power and regen depending on throttle
+    if x.TH >= 0
+        % baseline power torque
+        % torque limit after current, power, thermal derating
+        y = get_BL_PO(p, x, y);
+    elseif x.TH < 0
+        % baseline regen torque
+        % torque limit after speed, current, voltage, SOC, thermal derating
+        y = get_BL_RG(p, x, y);
+    end
 end
