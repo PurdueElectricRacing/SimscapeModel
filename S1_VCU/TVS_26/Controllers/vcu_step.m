659b45b58dcbfc1f2c501a2fac937ceff84f2e78
arclnx <73797060+arclnx@users.noreply.github.com>
2026-04-09 11:51:21 -0400

    create vcu_step

---
+++ S1_VCU/TVS_26/Controllers/vcu_step.m
@@ -0,0 +1,38 @@
+%% Function Description
+% vcu_step runs every loop on the TV board
+%
+% Inputs
+%   p   vehicle paramater struct. constant
+%   x   Raw sensor data struct. filled with data read from CAN
+%           in main.c
+%   y   Function input and output struct. contains all clipped and
+%           filtered variables, variable buffers, and output from
+%           this function.
+% Outputs
+%   y   modified version of input y
+
+function y = vcu_step(p, x, y)
+    % clip and filter results
+    y = get_CF(p, x, y);
+
+    % determine VCU mode
+    % y = get_VCU_mode(p,f,x,y);
+    
+    % switch between power and regen depending on throttle
+    if y.TH > 0
+        % equal torque (
+        % baseline power torque
+        % torque limit after current, power, thermal derating
+        y = get_BL_PO(p, y);
+
+        % set baseline to output torque
+        y.TORQUE_OUT = y.TO_BL_PO;
+    elseif y.TH == 0
+        y.TORQUE_OUT = [0 0 0 0];
+    elseif y.TH < 0
+        % baseline regen torque
+        % torque limit after speed, current, voltage, SOC, thermal derating
+        y = get_BL_RG(p, y);
+        y.TORQUE_OUT = y.TO_BL_RG;
+    end
+end
