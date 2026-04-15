3b5b1239fa6d40c871e58d2a7f0eca172e8ad2e1
arclnx <73797060+arclnx@users.noreply.github.com>
2026-04-15 10:53:13 -0400

    change how accel LUT is calculated

--- S1_VCU/TVS_26/Controllers/pVCU_master.m
+++ S1_VCU/TVS_26/Controllers/pVCU_master.m
@@ -6,13 +6,13 @@ properties
     ht; % half-track Unit: [m] Size [1 2] Order: [front rear]
     wb; % length of wheelbase Unit: [m] Size[1]
     gr; % gear ratio: tire speed * gr = motor shaft speed Unit: [unitless] Size: [1 1]
+    MAX_ABS_WM; % absolaute maximum motor speed Units: [rad/s] Size [1 1];
 
     % clip and filter raw signal parameters
     IB_AVG_length; % length of moving average window for battery current draw Unit: [# elements] Size: [1 1]
     
     % Power Baseline (get_BL_PO) parameters
     MAX_TO_ABS_PO; % absolute maxium torque Unit: [Nm] Size: [1 1]
-    MAX_ABS_WM; % absolaute maximum motor speed Units: [rad/s] Size [1 1];
     PB_derating_full_T;    % battery power draw when torque derating starts Unit: [kW] Size: [1 1]
     PB_derating_half_T;    % battery power draw when torque derates to half Unit: [kW] Size: [1 1]
     PB_derating_FR;       % 80kW limit derating F:R bias, 0 = only derate front, 1 = only derate rear Unit:[] Size: [1 1]
@@ -59,13 +59,13 @@ function p = pVCU_master()
     p.ht = [0.6490, 0.6210];
     p.wb = 2;
     p.gr = 12.51;
+    p.MAX_ABS_WM = 500;
 
     % clip and filter raw signal parameters
     p.IB_AVG_length = 10;
 
     % Baseline (get_BL) parameters
     p.MAX_TO_ABS_PO = 21;
-    p.MAX_ABS_WM = 500;
     p.PB_derating_full_T = 75;
     p.PB_derating_half_T = 80;
     p.PB_derating_FR = 0.75;
@@ -96,14 +96,17 @@ function p = pVCU_master()
 
     % Accel controller parameters
     % 1d look table for accel speed control
-    AC_low_GS = 10;
-    AC_high_GS = 50;
-    AC_high_slip = 1.2;
-    AC_low_W = AC_low_GS / p.r * AC_high_slip;
-    p.AC_speed_brkpt = [0, AC_low_GS, AC_high_GS];
-    p.AC_speed_table = [AC_low_W, AC_low_W, AC_high_slip * AC_high_GS / p.r];
-    p.AC_brkpt_lb = min(p.AC_speed_brkpt);
-    p.AC_brkpt_ub = max(p.AC_speed_brkpt);
+    AC_low_WW = 70; % constant wheelspeed at low speeds Units: [rad/s]
+    AC_high_SR = 1.1; % Slip ratio at high speeds Units: []
+    AC_high_offset = 0; % Vertical offset of slip ratio at high speeds Units: [rad/s]
+
+        AC_corner_GS = (AC_low_WW-AC_high_offset) / AC_high_SR; % groundspeed at transistion
+        AC_top_GS = p.MAX_ABS_WM / p.gr * p.r; % groundpseed at max motor speed
+        AC_top_WW = AC_top_GS * AC_high_SR / p.r + AC_high_offset; % calculated wheelspeed at max motor speed
+        p.AC_speed_brkpt = [0, AC_corner_GS, AC_top_GS];
+        p.AC_speed_table = [AC_low_WW, AC_low_WW, AC_top_WW];
+        p.AC_brkpt_lb = min(p.AC_speed_brkpt);
+        p.AC_brkpt_ub = max(p.AC_speed_brkpt);
 end
 end
 end
