266fa1ef6eb6f43f1a8fc7980bd90a4e977bf601
arclnx <73797060+arclnx@users.noreply.github.com>
2026-05-05 21:45:00 -0400

    update parse

--- S1_VCU/TVS_26/Car Testing Data/parse_data.m
+++ S1_VCU/TVS_26/Car Testing Data/parse_data.m
@@ -1,9 +1,16 @@
 function [data, header] = parse_data(folderpath, cols)
+    % options for all
     opts = detectImportOptions(folderpath);
     opts.SelectedVariableNames = cols;
+
+    % data
     optsdata = setvartype(opts, opts.VariableNames, 'double');
-    optsdata.DataLines = 5;
+    optsdata.DataLines = 5; % data starts on line 5
     data = readmatrix(folderpath, optsdata);
+
+    % headers
     optsheader = setvartype(opts, opts.VariableNames, 'string');
     optsheader.DataLines = [1 4];
     header = readmatrix(folderpath, optsheader, OutputType="string");
+
+    % 
